64da54d31d0e1845b97946e2e6e31f5ddfdb91cb
MikuroXina <ryosukadnak@gmail.com>
2022-01-23 01:39:59 +0900

    Improve MessageProxy

--- src/adaptor/message-proxy.ts
+++ src/adaptor/message-proxy.ts
@@ -1,5 +1,11 @@
 import { Client, Message, PartialMessage } from 'discord.js';
 import { MessageEventProvider } from '../runner';
+import type { AllMessageEventBoundary } from '../service';
+
+const compose =
+  <A, B, C>(g: (b: B) => C, f: (a: A) => B): ((a: A) => C) =>
+  (a) =>
+    g(f(a));
 
 /**
  * `Message` を受け渡す場合の `MessageEventProvider` を実装したクラス。
@@ -8,22 +14,27 @@ import { MessageEventProvider } from '../runner';
  * @class MessageProxy
  * @implements {MessageEventProvider<Message>}
  */
-export class MessageProxy implements MessageEventProvider<Message> {
-  constructor(private readonly client: Client) {}
+export class MessageProxy<M extends AllMessageEventBoundary>
+  implements MessageEventProvider<M>
+{
+  constructor(
+    private readonly client: Client,
+    private readonly map: (message: Message) => M
+  ) {}
 
-  onMessageCreate(handler: (message: Message) => Promise<void>): void {
-    this.client.on('messageCreate', handler);
+  onMessageCreate(handler: (message: M) => Promise<void>): void {
+    this.client.on('messageCreate', compose(handler, this.map));
   }
 
-  onMessageUpdate(handler: (message: Message) => Promise<void>): void {
+  onMessageUpdate(handler: (message: M) => Promise<void>): void {
     this.client.on('messageUpdate', async (message) =>
-      handler(await message.fetch())
+      compose(handler, this.map)(await message.fetch())
     );
   }
 
-  onMessageDelete(handler: (message: Message) => Promise<void>): void {
+  onMessageDelete(handler: (message: M) => Promise<void>): void {
     const wrapper = async (message: Message | PartialMessage) =>
-      handler(await message.fetch());
+      compose(handler, this.map)(await message.fetch());
 
     this.client.on('messageDelete', wrapper);
     this.client.on('messageDeleteBulk', async (messages) => {
