af9a90818588efaafe6cebcf753f7f8f8b94ae01
MikuroXina <ryosukadnak@gmail.com>
2022-01-23 01:50:33 +0900

    Remove register and Reexport

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,13 +1,1 @@
-import type { MessageResponseRunner, MessageEventResponder } from '../runner';
-import { Mitetazo, Observable } from './mitetazo';
-
-export type AllMessageEventBoundary = Observable;
-
-export function registerResponders<M extends AllMessageEventBoundary>(
-  runner: MessageResponseRunner<M>
-) {
-  const responders: MessageEventResponder<M>[] = [new Mitetazo()];
-  for (const responder of responders) {
-    runner.addResponder(responder);
-  }
-}
+export * from './mitetazo';
