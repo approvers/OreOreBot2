a1ccfb9930803f641440cf27f2b1d9abcd39dfc8
MikuroXina <ryosukadnak@gmail.com>
2022-01-30 02:43:39 +0900

    Add EditingObserver

---
+++ src/service/editing-observer.ts
@@ -0,0 +1,68 @@
+import { diffLines } from 'diff';
+import type { MessageEvent, MessageEventResponder } from '../runner';
+
+/**
+ * 監視するメッセージの抽象。
+ *
+ * @export
+ * @interface Observable
+ */
+export interface Observable {
+  /**
+   * メッセージの編集前の文章。
+   *
+   * @type {string}
+   * @memberof Observable
+   */
+  readonly before: string;
+  /**
+   * メッセージの編集後の文章。
+   *
+   * @type {string}
+   * @memberof Observable
+   */
+  readonly after: string;
+
+  /**
+   * `message` のメッセージをこのメッセージと同じチャンネルに送信する。
+   *
+   * @param {string} message
+   * @returns {Promise}
+   * @memberof Observable
+   */
+  sendToSameChannel(message: string): Promise<void>;
+}
+
+const diffComposer = (before: string, after: string): string => {
+  const changes = diffLines(before, after);
+  let composed = '';
+  for (const { value, added, removed } of changes) {
+    if (composed !== '') {
+      composed += '---------------------------------\n';
+    }
+    if (removed) {
+      composed += `- ${value}`;
+    }
+    if (added) {
+      composed += `+ ${value}`;
+    }
+  }
+  return composed;
+};
+
+export class EditingObserver implements MessageEventResponder<Observable> {
+  async on(event: MessageEvent, message: Observable): Promise<void> {
+    if (event !== 'UPDATE') {
+      return;
+    }
+    const { before, after } = message;
+    const composed = diffComposer(before, after);
+    if (composed === '') {
+      return;
+    }
+    await message.sendToSameChannel(`見てたぞ
+\`\`\`diff
+${composed}
+\`\`\``);
+  }
+}
