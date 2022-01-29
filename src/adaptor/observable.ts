37670c003f1284e685b89bb67c7c07315fe14883
MikuroXina <ryosukadnak@gmail.com>
2022-01-30 02:52:22 +0900

    Add editingObservableMessage

--- src/adaptor/observable.ts
+++ src/adaptor/observable.ts
@@ -1,5 +1,6 @@
 import type { Message } from 'discord.js';
 import type { DeletionObservable } from '../service/deletion-repeater';
+import type { EditingObservable } from '../service/editing-observer';
 
 export const deletionObservableMessage = (
   raw: Message
@@ -8,5 +9,16 @@ export const deletionObservableMessage = (
   content: raw.content,
   async sendToSameChannel(message: string): Promise<void> {
     await raw.channel.send(message);
+  }
+});
+
+export const editingObservableMessage = (
+  before: Message,
+  after: Message
+): EditingObservable => ({
+  before: before.content,
+  after: after.content,
+  async sendToSameChannel(message: string): Promise<void> {
+    await after.channel.send(message);
   }
 });
