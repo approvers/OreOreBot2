7bca2724b0b4809adedbbf882f29e340a5fc1d08
MikuroXina <ryosukadnak@gmail.com>
2022-01-30 16:30:05 +0900

    Imporve CommandMessage

--- src/service/command-observable.ts
+++ src/service/command-observable.ts
@@ -1,3 +1,5 @@
+import type { Snowflake } from '../model/id';
+
 /**
  * コマンド形式のメッセージの抽象。
  *
@@ -5,5 +7,28 @@
  * @interface CommandMessage
  */
 export interface CommandMessage {
+  /**
+   * コマンドの送信者の ID。
+   *
+   * @type {Snowflake}
+   * @memberof CommandMessage
+   */
+  sender: Snowflake;
+
+  /**
+   * コマンドの引数リスト。
+   *
+   * @type {readonly string[]}
+   * @memberof CommandMessage
+   */
   args: readonly string[];
+
+  /**
+   * このメッセージに `message` の内容で返信する。
+   *
+   * @param message
+   * @type {readonly string[]}
+   * @memberof CommandMessage
+   */
+  reply(message: string): Promise<void>;
 }
