7ea5e795e3db0ed51805194d19da79f55a2d77d8
MikuroXina <ryosukadnak@gmail.com>
2022-01-30 16:45:16 +0900

    Add InMemoryTypoRepository

---
+++ src/adaptor/in-memory-typo.ts
@@ -0,0 +1,26 @@
+import type { Snowflake } from '../model/id';
+import type { TypoRepository } from '../service/typo-record';
+
+export class InMemoryTypoRepository implements TypoRepository {
+  private dict: Map<Snowflake, string[]> = new Map();
+
+  private entry(id: Snowflake): string[] {
+    if (!this.dict.has(id)) {
+      this.dict.set(id, []);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return this.dict.get(id)!;
+  }
+
+  addTypo(id: Snowflake, newTypo: string): Promise<void> {
+    this.entry(id).push(newTypo);
+    return Promise.resolve();
+  }
+  allTyposByDate(id: Snowflake): Promise<readonly string[]> {
+    return Promise.resolve(this.entry(id));
+  }
+  clear(): Promise<void> {
+    this.dict = new Map();
+    return Promise.resolve();
+  }
+}
--- src/adaptor/index.ts
+++ src/adaptor/index.ts
@@ -2,3 +2,4 @@ export * from './message-proxy';
 export * from './voice-room-proxy';
 export * from './observable';
 export * from './clock';
+export * from './in-memory-typo';
