07d9efe98696285021d1dd4202bc44b3c901ca4a
MikuroXina <ryosukadnak@gmail.com>
2022-01-30 16:52:32 +0900

    Support TypoObservable

--- src/adaptor/observable.ts
+++ src/adaptor/observable.ts
@@ -1,10 +1,13 @@
 import type { Message, PartialMessage } from 'discord.js';
+import type { Snowflake } from '../model/id';
 import type { DeletionObservable } from '../service/deletion-repeater';
 import type { EditingObservable } from '../service/difference-detector';
+import type { TypoObservable } from '../service/typo-record';
 
 export const observableMessage = (
   raw: Message | PartialMessage
-): EditingObservable & DeletionObservable => ({
+): EditingObservable & DeletionObservable & TypoObservable => ({
+  id: (raw.author?.id || 'unknown') as Snowflake,
   author: raw.author?.username || '名無し',
   content: raw.content || '',
   async sendToSameChannel(message: string): Promise<void> {
