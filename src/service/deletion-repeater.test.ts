4719da72c85cb1d6ec2fe27d30d0aa091d2c3f0c
MikuroXina <ryosukadnak@gmail.com>
2022-02-05 00:58:23 +0900

    Add test for DeletionRepeater

---
+++ src/service/deletion-repeater.test.ts
@@ -0,0 +1,31 @@
+import { assert } from 'console';
+import { DeletionRepeater } from './deletion-repeater';
+
+test('react to deleted message', async () => {
+  const responder = new DeletionRepeater();
+  await responder.on('DELETE', {
+    author: 'Baba',
+    content: 'Wall Is Stop',
+    sendToSameChannel: (message) => {
+      expect(message)
+        .toEqual(`Babaさん、メッセージを削除しましたね？私は見ていましたよ。内容も知っています。
+\`\`\`
+Wall Is Stop
+\`\`\``);
+      return Promise.resolve();
+    }
+  });
+});
+
+test('must not react', async () => {
+  const responder = new DeletionRepeater();
+  await responder.on('CREATE', {
+    author: 'Baba',
+    content: 'Wall Is Not Stop',
+    sendToSameChannel: () => {
+      // must not be called
+      assert(false);
+      return Promise.resolve();
+    }
+  });
+});
