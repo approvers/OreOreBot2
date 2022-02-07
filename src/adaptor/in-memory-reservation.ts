8f781394a1bec42dcebb29a165bfa5abdce2bd46
MikuroXina <ryosukadnak@gmail.com>
2022-02-07 18:19:55 +0900

    Add InMemoryReservationRepository

---
+++ src/adaptor/in-memory-reservation.ts
@@ -0,0 +1,33 @@
+import type { Reservation, ReservationTime } from '../model/reservation';
+import type {
+  ReservationRepository,
+  ReservationResult
+} from '../service/kaere';
+
+export class InMemoryReservationRepository implements ReservationRepository {
+  map: Map<string, Reservation> = new Map();
+
+  private mapTimeToKey(time: ReservationTime): string {
+    return `${time.hours}:${time.minutes}`;
+  }
+
+  all(): Promise<readonly Reservation[]> {
+    return Promise.resolve([...this.map.values()]);
+  }
+  reservationAt(time: ReservationTime): Promise<Reservation | null> {
+    return Promise.resolve(this.map.get(this.mapTimeToKey(time)) ?? null);
+  }
+  reserve(reservation: Reservation): Promise<ReservationResult> {
+    const key = this.mapTimeToKey(reservation.time);
+    if (this.map.has(key)) {
+      return Promise.resolve('Err');
+    }
+    this.map.set(key, reservation);
+    return Promise.resolve('Ok');
+  }
+  cancel(reservation: Reservation): Promise<ReservationResult> {
+    return Promise.resolve(
+      this.map.delete(this.mapTimeToKey(reservation.time)) ? 'Ok' : 'Err'
+    );
+  }
+}
