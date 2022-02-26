742807f6dce0379e0e1543493136b54bd0acf52f
merunno <merunno92@gmail.com>
2022-02-26 21:58:08 +0900

    fix: 二重命名の修正

--- src/model/embed-message.ts
+++ src/model/embed-message.ts
@@ -22,28 +22,3 @@ export interface EmbedMessage {
   url?: string;
   thumbnail?: EmbedMessageThumbnail;
 }
-
-export interface EmbedMessageField {
-  name: string;
-  value: string;
-  inline?: boolean;
-}
-
-export interface EmbedMessageThumbnail {
-  url: string;
-}
-
-export interface EmbedMessage {
-  author?: {
-    iconUrl?: string;
-    name: string;
-    url?: string;
-  };
-  color?: number;
-  description?: string;
-  fields?: EmbedMessageField[];
-  footer?: string;
-  title?: string;
-  url?: string;
-  thumbnail?: EmbedMessageThumbnail;
-}
