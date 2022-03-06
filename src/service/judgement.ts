9c2cd545c719811ced9510e69558d10835cf75a9
MikuroXina <ryosukadnak@gmail.com>
2022-03-07 02:44:33 +0900

    Add JudgementCommand

---
+++ src/service/judgement.ts
@@ -0,0 +1,85 @@
+import type { MessageEvent, MessageEventResponder } from '../runner';
+import type { CommandMessage } from './command-message';
+
+export interface RandomGenerator {
+  sleep(): Promise<void>;
+  uniform(from: number, to: number): number;
+}
+
+const JUDGEMENT_TITLE = '***†HARACHO ONLINE JUDGEMENT SYSTEM†***';
+
+export class JudgementCommand implements MessageEventResponder<CommandMessage> {
+  constructor(private readonly rng: RandomGenerator) {}
+
+  async on(event: MessageEvent, message: CommandMessage): Promise<void> {
+    if (event !== 'CREATE') {
+      return;
+    }
+
+    const [commandName, countArg = '5', result = 'AC', errorFromStartArg] =
+      message.args;
+    if (!['jd', 'judge'].includes(commandName)) {
+      return;
+    }
+    const count = parseInt(countArg, 10);
+    if (Number.isNaN(count) || count <= 0) {
+      await message.reply({
+        title: '回数の指定が正の整数じゃないよ。'
+      });
+      return;
+    }
+    const willBeAccepted = result === 'AC';
+
+    if (willBeAccepted) {
+      await this.accept(message, count);
+    } else {
+      await this.reject(message, count, errorFromStartArg, result);
+    }
+  }
+
+  private async accept(message: CommandMessage, count: number) {
+    const sent = await message.reply({
+      title: JUDGEMENT_TITLE,
+      description: `1 / ${count} WJ`
+    });
+
+    for (let i = 0; i < count - 2; ++i) {
+      await sent.edit({
+        title: JUDGEMENT_TITLE,
+        description: `${i + 1} / ${count} WJ`
+      });
+      await this.rng.sleep();
+    }
+    await sent.edit({
+      title: JUDGEMENT_TITLE,
+      description: 'AC'
+    });
+  }
+
+  private async reject(
+    message: CommandMessage,
+    count: number,
+    errorFromStartArg: string,
+    result: string
+  ) {
+    const sent = await message.reply({
+      title: JUDGEMENT_TITLE,
+      description: `1 / ${count} WJ`
+    });
+
+    const errorFromStart = errorFromStartArg == '-all';
+    const errorAt = errorFromStart ? 0 : this.rng.uniform(0, count);
+
+    for (let i = 0; i < count - 2; ++i) {
+      await sent.edit({
+        title: JUDGEMENT_TITLE,
+        description: `${i + 1} / ${count} ${errorAt < i ? result : 'WJ'}`
+      });
+      await this.rng.sleep();
+    }
+    await sent.edit({
+      title: JUDGEMENT_TITLE,
+      description: result
+    });
+  }
+}
