e4c236efa4aafcced2c08a77ba85221b40612713
merunno <merunno92@gmail.com>
2022-03-11 00:08:11 +0900

    add: Bold italic copのテストコードの追加

---
+++ src/service/bold-italic-cop.test.ts
@@ -0,0 +1,57 @@
+import { BoldItalicCopReporter } from './bold-italic-cop';
+
+test('use case of bold-italic-cop', async () => {
+  const responder = new BoldItalicCopReporter();
+  await responder.on('CREATE', {
+    content: '***foge***',
+    replyMessage(message: { content: string }): Promise<void> {
+      expect(message.content).toStrictEqual(
+        'Bold-Italic警察だ!!! <:haracho:684424533997912096>'
+      );
+      return Promise.resolve();
+    }
+  });
+});
+
+test('4 asterisk of bold-italic-cop', async () => {
+  const responder = new BoldItalicCopReporter();
+  await responder.on('CREATE', {
+    content: '****foge****',
+    replyMessage(message: { content: string }): Promise<void> {
+      expect(message.content).toStrictEqual(
+        'Bold-Italic警察だ!!! <:haracho:684424533997912096>'
+      );
+      return Promise.resolve();
+    }
+  });
+});
+
+test('4 asterisk', async () => {
+  const responder = new BoldItalicCopReporter();
+  const fn = jest.fn();
+  await responder.on('CREATE', {
+    content: '****hoge',
+    replyMessage: fn
+  });
+  expect(fn).not.toHaveBeenCalled();
+});
+
+test('2 asterisk', async () => {
+  const responder = new BoldItalicCopReporter();
+  const fn = jest.fn();
+  await responder.on('CREATE', {
+    content: '**hoge',
+    replyMessage: fn
+  });
+  expect(fn).not.toHaveBeenCalled();
+});
+
+test('bold', async () => {
+  const responder = new BoldItalicCopReporter();
+  const fn = jest.fn();
+  await responder.on('CREATE', {
+    content: '**hoge**',
+    replyMessage: fn
+  });
+  expect(fn).not.toHaveBeenCalled();
+});
