bec3da667b20399b8e9890c11ff6dfffaaf11756
merunno <merunno92@gmail.com>
2022-03-14 14:03:24 +0900

    add: 引数不足時のエラー追加

--- src/service/hukueki.ts
+++ src/service/hukueki.ts
@@ -16,6 +16,14 @@ export class Hukueki implements MessageEventResponder<CommandMessage> {
        * 困らないでよ
        */
       case 'hukueki': {
+        if (!arg) {
+          await message.reply({
+            title: '服役できなかった。',
+            description: '引数が不足してるみたいだ。'
+          });
+          return;
+        }
+
         const hukuekiContext =
           'ねぇ、将来何してるだろうね\n' +
           arg +
@@ -29,6 +37,14 @@ export class Hukueki implements MessageEventResponder<CommandMessage> {
        * だから僕はロリコンを辞めた - こるく (Music Video)
        */
       case 'lolicon': {
+        if (!arg) {
+          await message.reply({
+            title: 'こるくはロリコンをやめられなかった。',
+            description: '引数が不足してるみたいだ。'
+          });
+          return;
+        }
+
         await message.reply({
           description: `だから僕は${arg}を辞めた - ${message.senderName} (Music Video)`
         });
@@ -38,6 +54,14 @@ export class Hukueki implements MessageEventResponder<CommandMessage> {
        * 限界みたいな鯖に住んでる菱形はどうすりゃいいですか？
        */
       case 'dousureba': {
+        if (!arg) {
+          await message.reply({
+            title: 'どうしようもない。',
+            description: '引数が不足してるみたいだ。'
+          });
+          return;
+        }
+
         await message.reply({
           description: `限界みたいな鯖に住んでる${arg}はどうすりゃいいですか？`
         });
