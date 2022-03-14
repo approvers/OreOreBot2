25fedf37cca0251a63ecb2aa738fbc3c8288b7ec
MikuroXina <ryosukadnak@gmail.com>
2022-03-15 00:54:18 +0900

    Add test

---
+++ src/service/kawaemon-has-all-roles.test.ts
@@ -0,0 +1,26 @@
+import { KawaemonHasAllRoles } from './kawaemon-has-all-roles';
+import type { Snowflake } from '../model/id';
+
+const KAWAEMON_ID = '391857452360007680' as Snowflake;
+
+test('kawaemon get a new role', async () => {
+  const newRoleId = '18475613045613281703151' as Snowflake;
+  const addRole = jest.fn(() => Promise.resolve());
+  const sendEmbed = jest.fn(() => Promise.resolve());
+  const responder = new KawaemonHasAllRoles(
+    KAWAEMON_ID,
+    { addRole },
+    { sendEmbed }
+  );
+
+  await responder.on('CREATE', {
+    roleId: newRoleId,
+    name: 'UBオヂサン'
+  });
+
+  expect(addRole).toHaveBeenCalledWith(KAWAEMON_ID, newRoleId);
+  expect(sendEmbed).toHaveBeenCalledWith({
+    title: '***Kawaemon has given a new role***',
+    description: `「UBオヂサン」をかわえもんにもつけといたよ。`
+  });
+});
