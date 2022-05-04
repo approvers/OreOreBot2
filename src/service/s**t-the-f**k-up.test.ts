eef3a9df457b312c46d7b5faef4013e7948b0ae7
raiga0310_arch <31821@toyota.kosen-ac.jp>
2022-05-04 16:24:45 +0900

    test: はらちょ粛清機能のテストを追加

---
+++ src/service/s**t-the-f**k-up.test.ts
@@ -0,0 +1,68 @@
+import { type Sheriff, SheriffCommand } from "./s**t-the-f**k-up";
+import { createMockMessage } from "./command-message";
+
+test('use case of stfu', async () => {
+  const sheriff: Sheriff = {
+    executeMessage(channel, historyRange){
+      console.log(channel, historyRange);
+      return Promise.resolve();
+    }
+  };
+  const responder = new SheriffCommand(sheriff);
+  const fn = jest.fn();
+  await responder.on(
+    'CREATE',
+    createMockMessage(
+      {
+        args: ['sftu'],
+        reply: fn
+      })
+  );
+
+
+  expect(fn).not.toHaveBeenCalled();
+  });
+
+test('delete message', async () => {
+  const sheriff: Sheriff = {
+    executeMessage(channel, historyRange){
+      console.log(channel, historyRange);
+      return Promise.resolve();
+    }
+  };
+  const responder = new SheriffCommand(sheriff);
+  const fn = jest.fn();
+  await responder.on(
+    'DELETE',
+    createMockMessage(
+      {
+        args: ['sftu'],
+        reply: fn
+      })
+  );
+
+
+  expect(fn).not.toHaveBeenCalled();
+});
+
+test('other command', async () => {
+  const sheriff: Sheriff = {
+    executeMessage(channel, historyRange){
+      console.log(channel, historyRange);
+      return Promise.resolve();
+    }
+  };
+  const responder = new SheriffCommand(sheriff);
+  const fn = jest.fn();
+  await responder.on(
+    'CREATE',
+    createMockMessage(
+      {
+        args: ['sft'],
+        reply: fn
+      })
+  );
+
+
+  expect(fn).not.toHaveBeenCalled();
+});
