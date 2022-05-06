5d45ab751f83f6f495ac5623da24ce4e82d9118e
ライガー <44018535+raiga0310@users.noreply.github.com>
2022-05-07 00:03:54 +0900

    feat: はらちょ粛清機能の追加 (#158)
    * feat: はらちょ粛清機能の追加
    
    * test: はらちょ粛清機能のテストを追加
    
    * chore: フォーマットかけた
    
    * docs: ドキュメントコメントの追加
    
    * chore: 余計なコメントの削除
    
    * chore: SnowFlakeのimport文を型のみに
    
    * chore: チャネルの情報の抽出タイミングを直前に
    
    * feat: テストで引数検証の追加
    
    * chore: format忘れ
    
    * feat: テストの引数検証を前テストに適用
    
    * fix: 引数検証のテストケースに代入するコマンドのスペルミスを修正

---
+++ src/service/s**t-the-f**k-up.ts
@@ -0,0 +1,54 @@
+import type {
+  CommandMessage,
+  CommandResponder,
+  HelpInfo
+} from './command-message';
+import type { MessageEvent } from '../runner';
+import type { Snowflake } from '../model/id';
+
+/**
+ * 'SheriffCommandのための削除機能。
+ *
+ * @export
+ * @interface Sheriff
+ */
+export interface Sheriff {
+  /**
+   * 'channel' 内の 'historyRange' 件のメッセージ中の自身のメッセージを削除する。
+   *
+   * @param {Snowflake} channel
+   * @param {number} historyRange
+   */
+  executeMessage(channel: Snowflake, historyRange: number): Promise<void>;
+}
+/**
+ * 'sftu' コマンドではらちょの直近のメッセージを削除する・
+ *
+ * @export
+ * @class Sheriff
+ *
+ */
+export class SheriffCommand implements CommandResponder {
+  help: Readonly<HelpInfo> = {
+    title: '治安統率機構',
+    description: 'はらちょの治安維持コマンドだよ',
+    commandName: ['stfu'],
+    argsFormat: []
+  };
+
+  constructor(private readonly sheriff: Sheriff) {}
+
+  async on(event: MessageEvent, message: CommandMessage): Promise<void> {
+    if (event !== 'CREATE') {
+      return;
+    }
+
+    const [commandName] = message.args;
+    if (!this.help.commandName.includes(commandName)) {
+      return;
+    }
+
+    const channel = message.senderChannelId;
+    await this.sheriff.executeMessage(channel, 50);
+  }
+}
