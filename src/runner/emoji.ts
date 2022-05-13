d61db46150c7c2dc9873b3254c4d73c274cd7b19
merunno <merunno92@gmail.com>
2022-05-14 02:31:34 +0900

    feat: `Client#emojiUpdate` の受け取りを追加

--- src/runner/emoji.ts
+++ src/runner/emoji.ts
@@ -9,7 +9,7 @@ export const composeEmojiEventResponders = <E>(
 ): EmojiEventResponder<E> => ({
   async on(event, emoji) {
     await Promise.all(
-      responders.map((responders) => responders.on(event, emoji))
+      responders.map((responder) => responder.on(event, emoji))
     );
   }
 });
@@ -44,5 +44,45 @@ export class EmojiResponseRunner<
 
   getResponders(): readonly R[] {
     return this.responders;
+  }
+}
+
+export interface EmojiUpdateEventResponder<E> {
+  on(event: 'UPDATE', before: E, after: E): Promise<void>;
+}
+
+export const composeEmojiUpdateEventResponders = <E>(
+  ...responders: readonly EmojiUpdateEventResponder<E>[]
+): EmojiUpdateEventResponder<E> => ({
+  async on(event, before, after) {
+    await Promise.all(
+      responders.map((responder) => responder.on(event, before, after))
+    );
+  }
+});
+
+export interface EmojiUpdateEventProvider<E> {
+  onEmojiUpdate(handler: (before: E, after: E) => Promise<void>): void;
+}
+
+export class EmojiUpdateResponseRunner<E> {
+  constructor(provider: EmojiUpdateEventProvider<E>) {
+    provider.onEmojiUpdate((...args) => this.triggerEvent(args));
+  }
+
+  private async triggerEvent([before, after]: [E, E]): Promise<void> {
+    try {
+      await Promise.all(
+        this.responders.map((res) => res.on('UPDATE', before, after))
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  private responders: EmojiUpdateEventResponder<E>[] = [];
+
+  addResponder(responder: EmojiUpdateEventResponder<E>) {
+    this.responders.push(responder);
   }
 }
