026c8b5cf614903da27103f8b5858f823982a32e
merunno <merunno92@gmail.com>
2022-05-29 02:46:41 +0900

    feat: delete, updateの処理を削除

--- src/runner/emoji.ts
+++ src/runner/emoji.ts
@@ -1,4 +1,4 @@
-export type EmojiEvent = 'CREATE' | 'DELETE';
+export type EmojiEvent = 'CREATE';
 
 export interface EmojiEventResponder<E> {
   on(event: EmojiEvent, emoji: E): Promise<void>;
@@ -16,7 +16,6 @@ export const composeEmojiEventResponders = <E>(
 
 export interface EmojiEventProvider<E> {
   onEmojiCreate(handler: (emoji: E) => Promise<void>): void;
-  onEmojiDelete(handler: (emoji: E) => Promise<void>): void;
 }
 
 export class EmojiResponseRunner<
@@ -25,7 +24,6 @@ export class EmojiResponseRunner<
 > {
   constructor(provider: EmojiEventProvider<E>) {
     provider.onEmojiCreate((emoji) => this.triggerEvent('CREATE', emoji));
-    provider.onEmojiDelete((emoji) => this.triggerEvent('DELETE', emoji));
   }
 
   private async triggerEvent(event: EmojiEvent, emoji: E): Promise<void> {
@@ -44,45 +42,5 @@ export class EmojiResponseRunner<
 
   getResponders(): readonly R[] {
     return this.responders;
-  }
-}
-
-export interface EmojiUpdateEventResponder<E> {
-  on(event: 'UPDATE', before: E, after: E): Promise<void>;
-}
-
-export const composeEmojiUpdateEventResponders = <E>(
-  ...responders: readonly EmojiUpdateEventResponder<E>[]
-): EmojiUpdateEventResponder<E> => ({
-  async on(event, before, after) {
-    await Promise.all(
-      responders.map((responder) => responder.on(event, before, after))
-    );
-  }
-});
-
-export interface EmojiUpdateEventProvider<E> {
-  onEmojiUpdate(handler: (before: E, after: E) => Promise<void>): void;
-}
-
-export class EmojiUpdateResponseRunner<E> {
-  constructor(provider: EmojiUpdateEventProvider<E>) {
-    provider.onEmojiUpdate((...args) => this.triggerEvent(args));
-  }
-
-  private async triggerEvent([before, after]: [E, E]): Promise<void> {
-    try {
-      await Promise.all(
-        this.responders.map((res) => res.on('UPDATE', before, after))
-      );
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  private responders: EmojiUpdateEventResponder<E>[] = [];
-
-  addResponder(responder: EmojiUpdateEventResponder<E>) {
-    this.responders.push(responder);
   }
 }
