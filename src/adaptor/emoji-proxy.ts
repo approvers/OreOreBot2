5ce3d5744e13a148eb738910f46aaa747e7bb841
merunno <merunno92@gmail.com>
2022-05-29 03:39:21 +0900

    feat: emojiProxyの追加

---
+++ src/adaptor/emoji-proxy.ts
@@ -0,0 +1,18 @@
+import { Client } from 'discord.js';
+import { EmojiData } from '../service/emoji-log';
+import { EmojiEventProvider } from '../runner';
+
+export type EmojiHandler<E> = (emoji: E) => Promise<void>;
+
+export class EmojiProxy implements EmojiEventProvider<EmojiData> {
+  constructor(private readonly client: Client) {}
+
+  onEmojiCreate(handler: EmojiHandler<EmojiData>): void {
+    this.client.on('emojiCreate', (emoji) =>
+      handler({
+        emoji: emoji.toString(),
+        emojiAuthorId: emoji.author?.id ?? '不明'
+      })
+    );
+  }
+}
