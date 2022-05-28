a76f91034e1d793f03e1e88bde1130bde59abcf6
merunno <merunno92@gmail.com>
2022-05-29 04:13:08 +0900

    feat: 作成者がわからない場合のエラー回避の追加

--- src/service/emoji-log.ts
+++ src/service/emoji-log.ts
@@ -3,13 +3,21 @@ import { StandardOutput } from './output';
 
 export interface EmojiData {
   emoji: string;
-  emojiAuthorId: string;
+  emojiAuthorId: string | undefined;
 }
 
 export class EmojiLog implements EmojiEventResponder<EmojiData> {
   constructor(private readonly output: StandardOutput) {}
   async on(event: RoleEvent, role: EmojiData): Promise<void> {
     if (event !== 'CREATE') {
+      return;
+    }
+
+    if (role.emojiAuthorId == undefined) {
+      await this.output.sendEmbed({
+        title: '絵文字警察',
+        description: `誰かが ${role.emoji} を作成しました`
+      });
       return;
     }
 
