81302bea73a72bec4e23e485712f11e2af32b5d9
MikuroXina <ryosukadnak@gmail.com>
2022-06-05 01:10:50 +0900

    fix: Enforce test

--- src/service/kawaemon-has-all-roles.test.ts
+++ src/service/kawaemon-has-all-roles.test.ts
@@ -20,6 +20,7 @@ test('kawaemon get a new role', async () => {
   });
 
   expect(addRole).toHaveBeenCalledWith(KAWAEMON_ID, newRoleId);
+  expect(removeRole).not.toHaveBeenCalled();
   expect(sendEmbed).toHaveBeenCalledWith({
     title: '***Kawaemon has given a new role***',
     description: `<@&18475613045613281703151>をかわえもんにもつけといたよ。`
@@ -42,9 +43,31 @@ test('kawaemon drop a new role', async () => {
     name: '限界ポイント8000超え'
   });
 
+  expect(addRole).not.toHaveBeenCalled();
   expect(removeRole).toHaveBeenCalledWith(KAWAEMON_ID, updatedRoleId);
   expect(sendEmbed).toHaveBeenCalledWith({
     title: '***Kawaemon has dropped a new role***',
     description: `<@&18475613045613281703151>をかわえもんからはずしといたよ。`
   });
+});
+
+test('must not drop a new role', async () => {
+  const updatedRoleId = '18475613045613281703151' as Snowflake;
+  const addRole = jest.fn(() => Promise.resolve());
+  const removeRole = jest.fn(() => Promise.resolve());
+  const sendEmbed = jest.fn(() => Promise.resolve());
+  const responder = new KawaemonHasAllRoles(
+    KAWAEMON_ID,
+    { addRole, removeRole },
+    { sendEmbed }
+  );
+
+  await responder.on('UPDATE', {
+    roleId: updatedRoleId,
+    name: 'YEAH'
+  });
+
+  expect(addRole).not.toHaveBeenCalled();
+  expect(removeRole).not.toHaveBeenCalled();
+  expect(sendEmbed).not.toHaveBeenCalled();
 });
