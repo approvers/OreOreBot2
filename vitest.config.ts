08a9b9ebb1b2561fea996891c99cc2b177002fe7
MikuroXina <ryosukadnak@gmail.com>
2022-06-09 19:03:05 +0900

    ci: Fix coverage reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'text-lcov'],
       reportsDirectory: 'coverage'
     }
   }
