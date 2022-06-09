3493b10f4ec75d405c9ce0d2e7a831b0c27566e4
Kisaragi <48310258+KisaragiEffective@users.noreply.github.com>
2022-06-09 12:59:57 +0000

    chore: add new tests

--- src/service/deletion-repeater.test.ts
+++ src/service/deletion-repeater.test.ts
@@ -27,3 +27,34 @@ it('must not react', async () => {
   });
   expect(fn).not.toHaveBeenCalled();
 });
+
+test("must not react if it's ignore target", async () => {
+  const responder = new DeletionRepeater(
+    (content) => content === 'Wall Is Stop'
+  );
+  const fn = jest.fn();
+  await responder.on('DELETE', {
+    author: 'Baba',
+    content: 'Wall Is Stop',
+    sendToSameChannel: fn
+  });
+  expect(fn).not.toHaveBeenCalled();
+});
+
+test("must react if it's not ignore target", async () => {
+  const responder = new DeletionRepeater(
+    (content) => content === 'Wall Is Stop'
+  );
+  await responder.on('DELETE', {
+    author: 'Baba',
+    content: 'Wall Is Stop',
+    sendToSameChannel: (message) => {
+      expect(message)
+        .toEqual(`Babaさん、メッセージを削除しましたね？私は見ていましたよ。内容も知っています。
+\`\`\`
+Wall Is Stop
+\`\`\``);
+      return Promise.resolve();
+    }
+  });
+});
