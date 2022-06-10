63708986d6078ef218d987e0c08d4829a7588941
Mikuroさいな <ryosukadnak@gmail.com>
2022-06-11 08:27:06 +0900

    Revert "feat: 限界ポイントロールをかわえもんから自動で外すように (#234)" (#252)
    This reverts commit 883b364f7a8e9828abc1f772e016a3cdfad45b2d.

--- src/service/kawaemon-has-all-roles.test.ts
+++ src/service/kawaemon-has-all-roles.test.ts
@@ -36,31 +36,6 @@ describe('kawaemon has all roles', () => {
     });
   });
 
-  it('kawaemon drop a new role', async () => {
-    const updatedRoleId = '18475613045613281703151' as Snowflake;
-    const addRole = vi.spyOn(manager, 'addRole');
-    const removeRole = vi.spyOn(manager, 'removeRole');
-    const sendEmbed = vi.spyOn(output, 'sendEmbed');
-
-    await responder.on('UPDATE', {
-      roleId: updatedRoleId,
-      name: '限界ポイント8000超え'
-    });
-
-    expect(addRole).not.toHaveBeenCalled();
-    expect(removeRole).toHaveBeenCalledWith(KAWAEMON_ID, updatedRoleId);
-    expect(sendEmbed).toHaveBeenCalledWith({
-      title: '***Kawaemon has dropped a new role***',
-      description: `<@&18475613045613281703151>をかわえもんからはずしといたよ。`,
-      fields: [
-        {
-          name: '理由',
-          value: '限界ポイントと名のつくロールは自動で外すよ。'
-        }
-      ]
-    });
-  });
-
   it('must not drop a new role', async () => {
     const updatedRoleId = '18475613045613281703151' as Snowflake;
     const addRole = vi.spyOn(manager, 'addRole');
