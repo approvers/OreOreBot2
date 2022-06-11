3d7b6b8d6f589c4c2ff3515861b0d0a717ae527d
MikuroXina <ryosukadnak@gmail.com>
2022-06-11 17:51:18 +0900

    fix: Improve fetcher

--- src/adaptor/version/fetch.ts
+++ src/adaptor/version/fetch.ts
@@ -2,15 +2,11 @@ import type { VersionFetcher } from '../../service/version.js';
 import { readFileSync } from 'node:fs';
 
 export class GenVersionFetcher implements VersionFetcher {
-  private _version: string;
+  public readonly version: string;
 
   constructor() {
-    this._version = readFileSync('./build/version.txt')
+    this.version = readFileSync('./build/version.txt')
       .toString('utf-8', 0, 100)
       .trim();
-  }
-
-  get version(): string {
-    return this._version;
   }
 }
