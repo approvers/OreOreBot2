6057ae853a8128b720a27238de06f3d2f98326c7
MikuroXina <ryosukadnak@gmail.com>
2022-06-28 22:49:02 +0900

    test: Add test for EmojiSeqReact

---
+++ src/service/emoji-seq-react.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it, vi } from 'vitest';
+import { EmojiSeqReact } from './emoji-seq-react.js';
+
+describe('EmojiSeqReact', () => {
+  const responder = new EmojiSeqReact();
+
+  it('adds the reaction to creation a message that contains Hibiki', async () => {
+    const addReaction = vi.fn<[string], Promise<void>>(() => Promise.resolve());
+    await responder.on('CREATE', {
+      content: '響き',
+      addReaction
+    });
+    expect(addReaction).toBeCalledTimes(1);
+    expect(addReaction).toHaveBeenCalledWith('<:haracho:684424533997912096>');
+  });
+
+  it('does not anything on deletion', async () => {
+    const addReaction = vi.fn<[string], Promise<void>>(() => Promise.resolve());
+    await responder.on('DELETE', {
+      content: '響',
+      addReaction
+    });
+    expect(addReaction).not.toHaveBeenCalled();
+  });
+});
