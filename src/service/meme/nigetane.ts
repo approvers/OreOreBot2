b665686701ffcceaaf156380c6736b3274741568
Nanai Jua <me@n7i.dev>
2022-07-13 01:29:34 +0900

    feat: add newlines

--- src/service/meme/nigetane.ts
+++ src/service/meme/nigetane.ts
@@ -1,7 +1,7 @@
 import type { MemeTemplate } from '../../model/meme-template.js';
 
 const template = (inject: string) =>
-  `--今、逃げたね。 逃げたでしょ。\n${inject}から悪くないって、正当化した。\n自分がいい子だって言い訳が見つかってよかったね。\nどんな気分？\n\n……手遅れなのは頭（おつむ）からなのかな。\nだって、 いつまでもそのまんまだよ。\n今だって、ずーっとそう。\n\n――ほんとあなたって、自分さえ良ければ良いんだね。`;
+  `――今、逃げたね。\n逃げたでしょ。\n${inject}から悪くないって、正当化した。\n\n自分がいい子だって言い訳が見つかってよかったね。\nどんな気分？\n\n……手遅れなのは頭（おつむ）からなのかな。\n\nだって、\nいつまでもそのまんまだよ。\n今だって、ずーっとそう。\n\n――ほんとあなたって、自分さえ良ければ良いんだね。`;
 
 export const nigetane: MemeTemplate<never, never> = {
   commandNames: ['nigetane'],
