7819b3b9fec4a2b6d3b2455608f1c706fdc03d4e
MikuroXina <ryosukadnak@gmail.com>
2022-07-21 02:34:49 +0900

    feat: Register command

--- src/adaptor/discord/member-stats.ts
+++ src/adaptor/discord/member-stats.ts
@@ -1,8 +1,14 @@
+import type {
+  MemberWithRole,
+  MembersWithRoleRepository
+} from '../../service/role-rank.js';
 import type { Client } from 'discord.js';
 import type { MemberStats } from '../../service/kokusei-chousa.js';
 import type { Snowflake } from '../../model/id.js';
 
-export class DiscordMemberStats implements MemberStats {
+export class DiscordMemberStats
+  implements MemberStats, MembersWithRoleRepository
+{
   constructor(
     private readonly client: Client,
     private readonly guildId: Snowflake
@@ -23,5 +29,17 @@ export class DiscordMemberStats implements MemberStats {
     }
     const guildMemberList = await guild.members.list({ limit: 1000 });
     return guildMemberList.filter((member) => member.user.bot).size;
+  }
+
+  async fetchMembersWithRole(): Promise<MemberWithRole[]> {
+    const guild = await this.client.guilds.fetch(this.guildId);
+    if (!guild) {
+      throw new Error('guild not found');
+    }
+    const guildMemberList = await guild.members.list({ limit: 1000 });
+    return guildMemberList.map(({ displayName, roles }) => ({
+      displayName,
+      roles: roles.cache.size
+    }));
   }
 }
