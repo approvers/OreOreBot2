34f20a9ebe19ff902dffc5916d05dc8d620b74b0
m2en <merunno92@gmail.com>
2022-08-05 17:22:48 +0900

    fix: move function

--- src/adaptor/discord/member-stats.ts
+++ src/adaptor/discord/member-stats.ts
@@ -2,12 +2,16 @@ import type {
   MemberWithRole,
   MembersWithRoleRepository
 } from '../../service/command/role-rank.js';
+import {
+  UserStats,
+  UserStatsRepository
+} from '../../service/command/user-info.js';
 import type { Client } from 'discord.js';
 import type { MemberStats } from '../../service/command/kokusei-chousa.js';
 import type { Snowflake } from '../../model/id.js';
 
 export class DiscordMemberStats
-  implements MemberStats, MembersWithRoleRepository
+  implements MemberStats, MembersWithRoleRepository, UserStatsRepository
 {
   constructor(
     private readonly client: Client,
@@ -41,5 +45,23 @@ export class DiscordMemberStats
       displayName,
       roles: roles.cache.size
     }));
+  }
+
+  async fetchStats(userId: string): Promise<UserStats | null> {
+    const guild = await this.client.guilds.fetch(this.guildId);
+    const member = await guild.members.fetch(userId);
+    if (!member) {
+      return null;
+    }
+
+    const joinedAt: Date | undefined = member.joinedAt ?? undefined;
+
+    return {
+      color: member.displayColor.toString(16).padStart(6, '0'),
+      displayName: member.displayName,
+      joinedAt: joinedAt,
+      bot: member.user.bot,
+      tag: member.user.tag
+    };
   }
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -45,7 +45,6 @@ import { DiscordMemberStats } from '../adaptor/discord/member-stats.js';
 import { DiscordMessageRepository } from '../adaptor/discord/message-repo.js';
 import { DiscordRoleManager } from '../adaptor/discord/role.js';
 import { DiscordSheriff } from '../adaptor/discord/sheriff.js';
-import { DiscordUserManager } from '../adaptor/discord/user.js';
 import { DiscordWS } from '../adaptor/discord/ws.js';
 import { GenVersionFetcher } from '../adaptor/version/fetch.js';
 import type { KaereMusicKey } from '../service/command/kaere.js';
@@ -136,7 +135,7 @@ const stats = new DiscordMemberStats(client, GUILD_ID as Snowflake);
 // ほとんど変わらないことが予想され環境変数で管理する必要性が薄いので、ハードコードした。
 const KAWAEMON_ID = '391857452360007680' as Snowflake;
 const roleManager = new DiscordRoleManager(client, GUILD_ID as Snowflake);
-const userManager = new DiscordUserManager(client, GUILD_ID as Snowflake);
+const userManager = new DiscordMemberStats(client, GUILD_ID as Snowflake);
 
 if (features.includes('COMMAND')) {
   registerAllCommandResponder({
