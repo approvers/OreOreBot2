c4f51b705836ece5f58d55fc40c401bc8168bc93
Meru ｜ m9eenn <me@merunno.dev>
2022-08-06 13:35:55 +0900

    Update src/service/command/user-info.test.ts
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

--- src/service/command/user-info.test.ts
+++ src/service/command/user-info.test.ts
@@ -1,6 +1,6 @@
 import { UserInfo, UserStatsRepository } from './user-info.js';
 import { afterEach, describe, expect, it, vi } from 'vitest';
-import { Snowflake } from '../../model/id.js';
+import type { Snowflake } from '../../model/id.js';
 import { createMockMessage } from './command-message.js';
 
 describe('UserInfo', () => {
