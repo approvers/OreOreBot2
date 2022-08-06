7f252a7d0f3d0d033d835b52e6f33726416d6619
m2en <merunno92@gmail.com>
2022-08-06 10:52:28 +0900

    fix: remove test case

--- src/service/command/user-info.test.ts
+++ src/service/command/user-info.test.ts
@@ -13,13 +13,13 @@ describe('UserInfo', () => {
       Promise.resolve(
         id === '586824421470109716'
           ? {
-              displayName: 'める',
-              tag: 'm2en#0092',
               color: 'f08f8f',
+              displayName: 'める',
               joinedAt: new Date(20050902),
               createdAt: new Date(20050902),
-              hoistRoleId: '865951894173515786' as Snowflake,
-              bot: false
+              bot: false,
+              tag: 'm2en#0092',
+              hoistRoleId: '865951894173515786' as Snowflake
             }
           : null
       )
@@ -106,132 +106,6 @@ describe('UserInfo', () => {
     expect(fn).toHaveBeenCalledWith({
       title: 'コマンド形式エラー',
       description: '引数にユーザーIDの文字列を指定してね'
-    });
-    expect(fetchStats).toHaveBeenCalledOnce();
-  });
-
-  it('error with top role undefined', async () => {
-    const fetchStats = vi.spyOn(repo, 'fetchStats');
-    const fn = vi.fn();
-
-    await userInfo.on(
-      'CREATE',
-      createMockMessage(
-        {
-          args: ['userinfo', '586824421470109716']
-        },
-        fn
-      )
-    );
-
-    expect(fn).toHaveBeenCalledWith({
-      title: 'ユーザーの情報',
-      description: '司令官、頼まれていた <@586824421470109716> の情報だよ',
-      fields: [
-        {
-          name: 'ID',
-          value: '586824421470109716',
-          inline: true
-        },
-        {
-          name: '表示名',
-          value: 'める',
-          inline: true
-        },
-        {
-          name: 'ユーザー名+Discord Tag',
-          value: 'm2en#0092',
-          inline: true
-        },
-        {
-          name: 'プロフィールカラー',
-          value: 'f08f8f',
-          inline: true
-        },
-        {
-          name: 'ユーザ種別',
-          value: '人類',
-          inline: true
-        },
-        {
-          name: 'メンバーリストロール(最上位)',
-          value: 'なし',
-          inline: true
-        },
-        {
-          name: '参加日時',
-          value: `<t:20050>(<t:20050:R>)`,
-          inline: true
-        },
-        {
-          name: 'アカウント作成日時',
-          value: `<t:20050>(<t:20050:R>)`,
-          inline: true
-        }
-      ]
-    });
-    expect(fetchStats).toHaveBeenCalledOnce();
-  });
-
-  it('error with joinedAt undefined', async () => {
-    const fetchStats = vi.spyOn(repo, 'fetchStats');
-    const fn = vi.fn();
-
-    await userInfo.on(
-      'CREATE',
-      createMockMessage(
-        {
-          args: ['userinfo', '586824421470109716']
-        },
-        fn
-      )
-    );
-
-    expect(fn).toHaveBeenCalledWith({
-      title: 'ユーザーの情報',
-      description: '司令官、頼まれていた <@586824421470109716> の情報だよ',
-      fields: [
-        {
-          name: 'ID',
-          value: '586824421470109716',
-          inline: true
-        },
-        {
-          name: '表示名',
-          value: 'める',
-          inline: true
-        },
-        {
-          name: 'ユーザー名+Discord Tag',
-          value: 'm2en#0092',
-          inline: true
-        },
-        {
-          name: 'プロフィールカラー',
-          value: 'f08f8f',
-          inline: true
-        },
-        {
-          name: 'ユーザ種別',
-          value: '人類',
-          inline: true
-        },
-        {
-          name: 'メンバーリストロール(最上位)',
-          value: 'なし',
-          inline: true
-        },
-        {
-          name: '参加日時',
-          value: `情報なし`,
-          inline: true
-        },
-        {
-          name: 'アカウント作成日時',
-          value: `<t:20050>(<t:20050:R>)`,
-          inline: true
-        }
-      ]
     });
     expect(fetchStats).toHaveBeenCalledOnce();
   });
