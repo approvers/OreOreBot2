caefd7fcfe969ac0942a1c7b68a5e2507179e238
m2en <me@m2en.dev>
2022-08-20 23:27:31 +0900

    feat: ロール作成コマンドの受け取りを追加

---
+++ src/service/command/role-create.ts
@@ -0,0 +1,64 @@
+import {
+  CommandMessage,
+  CommandResponder,
+  HelpInfo
+} from './command-message.js';
+import type { MessageEvent } from '../../runner/message.js';
+
+export interface RoleManager {
+  createRole(roleName: string, roleColor: string): Promise<void>;
+}
+
+export class RoleCreate implements CommandResponder {
+  help: Readonly<HelpInfo> = {
+    title: 'ロール作成',
+    description: 'ロールを作成するよ',
+    commandName: ['rolecreate'],
+    argsFormat: [
+      {
+        name: 'ロール名',
+        description: '作成するロールの名前を指定してね'
+      },
+      {
+        name: 'ロールの色',
+        description:
+          '作成するロールの色を[HEX](https://www.weblio.jp/content/Hex)で指定してね'
+      }
+    ]
+  };
+
+  constructor(private readonly manager: RoleManager) {}
+
+  async on(event: MessageEvent, message: CommandMessage): Promise<void> {
+    if (event !== 'CREATE') {
+      return;
+    }
+
+    const [command, roleName, roleColor] = message.args;
+    if (!this.help.commandName.includes(command)) {
+      return;
+    }
+
+    if (typeof roleName !== 'string') {
+      await message.reply({
+        title: 'コマンド形式エラー',
+        description: '引数にロール名の文字列を指定してね'
+      });
+      return;
+    }
+    if (typeof roleColor !== 'string') {
+      await message.reply({
+        title: 'コマンド形式エラー',
+        description:
+          '引数にロールの色の[HEX](https://www.weblio.jp/content/Hex)を指定してね'
+      });
+      return;
+    }
+
+    await this.manager.createRole(roleName, roleColor);
+    await message.reply({
+      title: 'ロール作成',
+      description: 'ロールを作成したよ'
+    });
+  }
+}
