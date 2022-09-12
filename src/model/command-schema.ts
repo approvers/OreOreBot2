027912e95675a2e6df62487ef604527841be6961
MikuroXina <ryosukadnak@gmail.com>
2022-09-12 13:34:00 +0900

    docs: Add docs and tidy up

--- src/model/command-schema.ts
+++ src/model/command-schema.ts
@@ -1,8 +1,22 @@
+/**
+ * 真偽値の引数のスキーマ。`defaultValue` が未定義ならば必須の引数になる。
+ *
+ * @export
+ * @interface BooleanParam
+ */
 export interface BooleanParam {
   type: 'BOOLEAN';
   defaultValue?: boolean;
 }
 
+/**
+ * 文字列の引数のスキーマ。`defaultValue` が未定義ならば必須の引数になる。
+ *
+ * `minLength` や `maxLength` で文字列長の最小値と最大値を指定できる。長さが指定された範囲の外ならばパースに失敗する。
+ *
+ * @export
+ * @interface StringParam
+ */
 export interface StringParam {
   type: 'STRING';
   defaultValue?: string;
@@ -10,11 +24,27 @@ export interface StringParam {
   maxLength?: number;
 }
 
+/**
+ * ユーザなどの ID の引数のスキーマ。`defaultValue` が未定義ならば必須の引数になる。
+ *
+ * @export
+ * @interface SnowflakeParam
+ */
 export interface SnowflakeParam {
   type: 'USER' | 'CHANNEL' | 'ROLE';
   defaultValue?: string;
 }
 
+/**
+ * 数値の引数のスキーマ。`defaultValue` が未定義ならば必須の引数になる。
+ *
+ * `type` が `INTEGER` のとき、数字以外を含む文字列ならばパースに失敗する。
+ *
+ * `minLength` や `maxLength` で数値の最小値と最大値を指定できる。長さが指定された範囲の外ならばパースに失敗する。
+ *
+ * @export
+ * @interface NumberParam
+ */
 export interface NumberParam {
   type: 'INTEGER' | 'FLOAT';
   defaultValue?: number;
@@ -22,6 +52,14 @@ export interface NumberParam {
   maxValue?: number;
 }
 
+/**
+ * 選択式の引数のスキーマ。`defaultValueIndex` が未定義ならば必須の引数になる。パース結果は、対応する `choices` 内の文字列のインデックスである。
+ *
+ * `choices` の中に存在しない文字列ならばパースに失敗する。
+ *
+ * @export
+ * @interface ChoicesParam
+ */
 export interface ChoicesParam {
   type: 'CHOICES';
   defaultValueIndex?: number;
@@ -35,6 +73,13 @@ export type Param =
   | NumberParam
   | ChoicesParam;
 export type ParamType = Param['type'];
+/**
+ * 引数のスキーマ `P` に対応するパース結果の型を返す。
+ *
+ * @export
+ * @typedef ParamValue
+ * @template P 引数のスキーマの型
+ */
 export type ParamValue<P extends Param> = P extends BooleanParam
   ? boolean
   : P extends NumberParam | ChoicesParam
@@ -45,29 +90,57 @@ export type ParamValueByKey<P> = {
   [K in keyof P]: P[K] extends Param ? ParamValue<P[K]> : Record<string, never>;
 };
 
+/**
+ * コマンドの中で分岐する細かいサブコマンド。
+ *
+ * @export
+ * @interface SubCommand
+ */
 export interface SubCommand {
   type: 'SUB_COMMAND';
-  params?: Readonly<Record<string, Param>>;
-}
-export interface SubCommandGroup {
-  type: 'SUB_COMMAND_GROUP';
-  params?: Readonly<Record<string, Param>>;
-  subCommands: Readonly<Record<string, SubCommand>>;
+  params: Readonly<Record<string, Param>>;
 }
 
+/**
+ * サブコマンドが属するグループ。これ単体ではコマンドとして実行できないが、グループ全体で共通する引数を定義できる。
+ *
+ * @export
+ * @interface SubCommandGroup
+ */
+export interface SubCommandGroup {
+  type: 'SUB_COMMAND_GROUP';
+  params: Readonly<Record<string, Param>>;
+  subCommands: Readonly<Record<string, SubCommand | SubCommandGroup>>;
+}
+
+/**
+ * コマンドの引数のスキーマ。
+ *
+ * `names` はこのコマンド実行に利用可能な全てのコマンド名、`params` はコマンド全体で共通の引数、`subCommands` はこの配下にあるサブコマンドである。
+ *
+ * @export
+ * @interface Schema
+ */
 export interface Schema {
   names: readonly string[];
   params: Readonly<Record<string, Param>>;
   subCommands: Readonly<Record<string, SubCommand | SubCommandGroup>>;
 }
 
+/**
+ * コマンドのスキーマ `S` に対応するパース結果の型を返す。
+ *
+ * @export
+ * @typedef ParsedSchema
+ * @template S コマンドスキーマの型
+ */
 export type ParsedSchema<S> = S extends Record<
   string,
   SubCommand | SubCommandGroup
 >
   ? {
       [K in keyof S]: {
-        name: K;
+        key: K;
         value: Params<S[K]>;
       };
     }[keyof S]
@@ -79,24 +152,11 @@ export type ParsedSchema<S> = S extends Record<
     }
   : never;
 
-export type Names<S extends Schema> = S['names'];
-export type Params<S extends Schema | SubCommand | SubCommandGroup> =
-  S['params'] &
-    (S extends infer T
-      ? T extends SubCommandGroup
-        ? ParsedSchema<SubCommands<T>>
-        : Record<string, never>
-      : Record<string, never>);
-export type SubCommands<S extends Schema | SubCommandGroup> = [S] extends [
-  infer T
-]
-  ? T extends Schema | SubCommandGroup
-    ? T['subCommands']
-    : never
-  : never;
-
-export type ParamsByCommand<
-  S extends Record<string, SubCommand | SubCommandGroup>
-> = {
-  [K in keyof S]: Params<S[K]>;
-};
+type Names<S extends Schema> = S['names'];
+type Params<S extends Schema | SubCommand | SubCommandGroup> = S['params'] &
+  (S extends infer T
+    ? T extends SubCommandGroup
+      ? ParsedSchema<SubCommands<T>>
+      : Record<string, never>
+    : Record<string, never>);
+type SubCommands<S extends Schema | SubCommandGroup> = S['subCommands'];
