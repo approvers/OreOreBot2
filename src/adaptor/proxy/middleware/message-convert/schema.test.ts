9a6d16776efc941f51a783b4f537dd32121f48e0
MikuroXina <ryosukadnak@gmail.com>
2022-09-14 01:12:52 +0900

    test: Enforce test cases

--- src/adaptor/proxy/middleware/message-convert/schema.test.ts
+++ src/adaptor/proxy/middleware/message-convert/schema.test.ts
@@ -2,53 +2,69 @@ import { expect, test } from 'vitest';
 
 import { parseStrings } from './schema.js';
 
-const TIME_OPTION = [{ name: 'at', type: 'STRING' }] as const;
+test('no args', () => {
+  const SERVER_INFO_SCHEMA = {
+    names: ['serverinfo'],
+    subCommands: {}
+  } as const;
 
-const SCHEMA = {
-  names: ['kaere'],
-  subCommands: {
-    start: {
-      type: 'SUB_COMMAND'
-    },
-    bed: {
-      type: 'SUB_COMMAND_GROUP',
-      subCommands: {
-        enable: {
-          type: 'SUB_COMMAND'
-        },
-        disable: {
-          type: 'SUB_COMMAND'
-        },
-        status: {
-          type: 'SUB_COMMAND'
+  const noParamRes = parseStrings(['serverinfo'], SERVER_INFO_SCHEMA);
+
+  expect(noParamRes).toStrictEqual([
+    'Ok',
+    {
+      name: 'serverinfo',
+      params: []
+    }
+  ]);
+});
+
+test('single arg', () => {
+  const TIME_OPTION = [{ name: 'at', type: 'STRING' }] as const;
+  const KAERE_SCHEMA = {
+    names: ['kaere'],
+    subCommands: {
+      start: {
+        type: 'SUB_COMMAND'
+      },
+      bed: {
+        type: 'SUB_COMMAND_GROUP',
+        subCommands: {
+          enable: {
+            type: 'SUB_COMMAND'
+          },
+          disable: {
+            type: 'SUB_COMMAND'
+          },
+          status: {
+            type: 'SUB_COMMAND'
+          }
         }
-      }
-    },
-    reserve: {
-      type: 'SUB_COMMAND_GROUP',
-      subCommands: {
-        add: {
-          type: 'SUB_COMMAND',
-          params: TIME_OPTION
-        },
-        cancel: {
-          type: 'SUB_COMMAND',
-          params: TIME_OPTION
-        },
-        list: {
-          type: 'SUB_COMMAND'
+      },
+      reserve: {
+        type: 'SUB_COMMAND_GROUP',
+        subCommands: {
+          add: {
+            type: 'SUB_COMMAND',
+            params: TIME_OPTION
+          },
+          cancel: {
+            type: 'SUB_COMMAND',
+            params: TIME_OPTION
+          },
+          list: {
+            type: 'SUB_COMMAND'
+          }
         }
       }
     }
-  }
-} as const;
+  } as const;
 
-test('single arg', () => {
-  const noParamRes = parseStrings(['kaere'], SCHEMA);
+  const noParamRes = parseStrings(['kaere'], KAERE_SCHEMA);
 
   expect(noParamRes).toStrictEqual(['Err', ['NEED_MORE_ARGS']]);
 
-  const oneParamRes = parseStrings(['kaere', 'start'], SCHEMA);
+  const oneParamRes = parseStrings(['kaere', 'start'], KAERE_SCHEMA);
 
   expect(oneParamRes).toStrictEqual([
     'Ok',
@@ -64,7 +80,7 @@ test('single arg', () => {
 
   const subCommandRes = parseStrings(
     ['kaere', 'reserve', 'add', '01:12'],
-    SCHEMA
+    KAERE_SCHEMA
   );
 
   expect(subCommandRes).toStrictEqual([
@@ -80,6 +96,34 @@ test('single arg', () => {
           params: ['01:12']
         }
       }
+    }
+  ]);
+});
+
+test('multi args', () => {
+  const ROLE_CREATE_SCHEMA = {
+    names: ['rolecreate'],
+    subCommands: {},
+    params: [
+      { type: 'USER', name: 'target' },
+      { type: 'STRING', name: 'color', defaultValue: 'random' }
+    ]
+  } as const;
+
+  const noParamRes = parseStrings(['rolecreate'], ROLE_CREATE_SCHEMA);
+
+  expect(noParamRes).toStrictEqual(['Err', ['NEED_MORE_ARGS']]);
+
+  const oneParamRes = parseStrings(
+    ['rolecreate', '0123456789'],
+    ROLE_CREATE_SCHEMA
+  );
+
+  expect(oneParamRes).toStrictEqual([
+    'Ok',
+    {
+      name: 'rolecreate',
+      params: ['0123456789', 'random']
     }
   ]);
 });
