b1999140ace789187f478b223217033a67e3b5d6
raiga <raiga0310@icloud.com>
2022-09-19 16:18:29 +0900

    feat: テストケースの追加 i.   通常時 ii.  引数に何も指定されなかった場合 iii. 大きすぎるダイスの実行時

---
+++ src/service/command/dice.test.ts
@@ -0,0 +1,63 @@
+import { DiceCommand, DiceQueen } from './dice.js';
+import { describe, expect, it, vi } from 'vitest';
+
+import { createMockMessage } from './command-message.js';
+import { parseStringsOrThrow } from '../../adaptor/proxy/command/schema.js';
+
+describe('dice', () => {
+  const diceQueen: DiceQueen = {
+    roll: (face, num) => [...new Array<undefined>(face)].map(() => num)
+  };
+  const diceCommand = new DiceCommand(diceQueen);
+
+  it('case of 1d6', async () => {
+    const roll = vi.spyOn(diceQueen, 'roll');
+    const fn = vi.fn();
+
+    await diceCommand.on(
+      createMockMessage(
+        parseStringsOrThrow(['dice', '1d6'], diceCommand.schema),
+        fn
+      )
+    );
+
+    expect(fn).toHaveBeenCalledWith({
+      title: '運命のダイスロール！',
+      description: '1d6 => 6'
+    });
+    expect(roll).toHaveBeenCalledOnce();
+  });
+
+  it('case of defaultValue', async () => {
+    const roll = vi.spyOn(diceQueen, 'roll');
+    const fn = vi.fn();
+
+    await diceCommand.on(
+      createMockMessage(parseStringsOrThrow(['dice'], diceCommand.schema), fn)
+    );
+
+    expect(fn).toHaveBeenCalledWith({
+      title: '運命のダイスロール！',
+      description: '1d100 => 100'
+    });
+    expect(roll).toHaveBeenCalledOnce();
+  });
+
+  it('case of 100D500', async () => {
+    const roll = vi.spyOn(diceQueen, 'roll');
+    const fn = vi.fn();
+
+    await diceCommand.on(
+      createMockMessage(
+        parseStringsOrThrow(['dice', '100D500'], diceCommand.schema),
+        fn
+      )
+    );
+
+    expect(fn).toHaveBeenCalledWith({
+      title: '引数が大きすぎるよ',
+      description: 'ダイスは20面100個以下、最大値が200までの処理にしてね。'
+    });
+    expect(roll).toBeCalledTimes(0);
+  });
+});
