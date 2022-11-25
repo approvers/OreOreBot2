575754cbcda025b8a5ee5e3d9d34536d82d67bf8
m2en <me@m2en.dev>
2022-11-25 13:05:40 +0000

    feat: add line separater

--- src/service/command/meme/koume.ts
+++ src/service/command/meme/koume.ts
@@ -8,9 +8,8 @@ export const koume: MemeTemplate<never, never> = {
   errorMessage:
     'MEMEを表示しようと思ったら〜♪ 引数が足りませんでした〜♪ チクショー！！',
   generate(args) {
-    // TODO: 改行する
     const option1 = args.body.split(' ')[0];
     const option2 = args.body.split(' ')[1];
-    return `${option1}と思ったら〜♪ ${option2}でした〜♪ チクショー！！`;
+    return `${option1}と思ったら〜♪\n${option2}でした〜♪\nチクショー！！`;
   }
 };
