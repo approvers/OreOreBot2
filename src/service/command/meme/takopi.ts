8942ace4085b279d85d5998e21e87632cdb5d7f9
raiga0310 <raiga0310brilio@gmail.com>
2022-11-26 13:24:23 +0900

    refactor: オプション引数への再設計( #586 対応待ち )

--- src/service/command/meme/takopi.ts
+++ src/service/command/meme/takopi.ts
@@ -1,8 +1,13 @@
 import type { MemeTemplate } from '../../../model/meme-template.js';
 
-const takopiFlags = ['f', 'c'] as const;
+const takopiFlags = ['f'] as const;
+//waiting merge #586
+const takopiOptions = ['c'] as const;
 
-export const takopi: MemeTemplate<typeof takopiFlags[number], never> = {
+export const takopi: MemeTemplate<
+  typeof takopiFlags[number],
+  typeof takopiOptions[number]
+> = {
   commandNames: ['takopi'],
   description:
     '「〜、出して」\n`-f` で教員と自分の名前の位置を反対にします。\n`-c`で教員の名前も変更可能です。\n([idea: フライさん](https://github.com/approvers/OreOreBot2/issues/90))',
@@ -10,10 +15,8 @@ export const takopi: MemeTemplate<typeof takopiFlags[number], never> = {
   optionsKeys: [],
   errorMessage: '(引数が)わ、わかんないっピ.......',
   generate(args, author) {
-    if (args.flags.c)
-      return `${author}「${args.body.split(' ')[0]}、出して」\n${
-        args.body.split(' ')[1]
-      }「わ、わかんないっピ.......」`;
+    if (args.options.c)
+      return `${author}「${args.body}、出して」\n${args.options.c}「わ、わかんないっピ.......」`;
     if (args.flags.f)
       return `${author}「${args.body}、出して」\n教員「わ、わかんないっピ.......」`;
     return `教員「${args.body}、出して」\n${author}「わ、わかんないっピ.......」`;
