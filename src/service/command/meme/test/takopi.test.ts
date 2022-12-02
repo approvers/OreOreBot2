8e389ea6364d717aff4a5de23c7a228f575cfad4
m2en <me@m2en.dev>
2022-12-02 09:18:12 +0000

    refactor: takopi tast-case to split

---
+++ src/service/command/meme/test/takopi.test.ts
@@ -0,0 +1,93 @@
+import { describe, expect, it } from 'vitest';
+import { Meme } from '../../meme.js';
+
+import { createMockMessage } from '../../command-message.js';
+import { parseStringsOrThrow } from '../../../../adaptor/proxy/command/schema.js';
+
+describe('meme', () => {
+  const responder = new Meme();
+
+  it('use case of takopi', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(['takopi', 'こるく'], responder.schema),
+        (message) => {
+          expect(message).toStrictEqual({
+            description: `教員「こるく、出して」\nりにあ「わ、わかんないっピ.......」`
+          });
+        },
+        {
+          senderName: 'りにあ'
+        }
+      )
+    );
+  });
+
+  it('use case of takopi (-f)', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(['takopi', '-f', 'こるく'], responder.schema),
+        (message) => {
+          expect(message).toStrictEqual({
+            description: `りにあ「こるく、出して」\n教員「わ、わかんないっピ.......」`
+          });
+        },
+        {
+          senderName: 'りにあ'
+        }
+      )
+    );
+  });
+
+  it('use case of takopi (-c)', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(
+          ['takopi', '-c', 'こるく', 'いっそう'],
+          responder.schema
+        ),
+        (message) => {
+          expect(message).toStrictEqual({
+            description: `こるく「いっそう、出して」\nりにあ「わ、わかんないっピ.......」`
+          });
+        },
+        {
+          senderName: 'りにあ'
+        }
+      )
+    );
+  });
+
+  it('use case of takopi (-f, -c)', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(
+          ['takopi', '-f', '-c', 'こるく', 'いっそう'],
+          responder.schema
+        ),
+        (message) => {
+          expect(message).toStrictEqual({
+            description: `りにあ「いっそう、出して」\nこるく「わ、わかんないっピ.......」`
+          });
+        },
+        {
+          senderName: 'りにあ'
+        }
+      )
+    );
+  });
+
+  it('few arguments of takopi (-c)', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(['takopi', '-c', 'こるく'], responder.schema),
+        (message) => {
+          expect(message).toStrictEqual({
+            description: '(引数が)わ、わかんないっピ.......',
+            title: '引数が不足してるみたいだ。'
+          });
+        }
+      )
+    );
+  });
+});
