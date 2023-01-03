7134575123f018992f7059a800c9125ffbf23ee7
m2en <me@m2en.dev>
2023-01-04 05:02:31 +0900

    docs: add FIXME comment

--- src/server/index.ts
+++ src/server/index.ts
@@ -146,6 +146,7 @@ if (features.includes('COMMAND')) {
       KAKAPO: join('assets', 'party', 'kakapo.mp3'),
       KAKUSIN_DAISUKE: join('assets', 'party', 'kakusin-daisuke.mp3'),
       NEROYO: join('assets', 'kaere', 'neroyo.mp3'),
+      // FIXME: 元々映像ファイルだったものをMacで変換しただけなので、他のアセットと合わせるためにmp3に変換すべきかもしれない
       GYOKUON: join('assets', 'gyokuon', 'gyokuon.m4a'),
       GYOKUON_SHORT: join('assets', 'gyokuon', 'gyokuon-short.m4a')
     }),
