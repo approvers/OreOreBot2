d923a3ad96e5da6163eb6c8b3d3a1dedcb0e6b98
m2en <me@m2en.dev>
2023-01-21 20:39:39 +0900

    feat: プログラマ狩り構文の追加 (#683)

---
+++ src/service/command/meme/tsureteike.ts
@@ -0,0 +1,33 @@
+import type { MemeTemplate } from '../../../model/meme-template.js';
+
+export const tsureteike: MemeTemplate<never, never> = {
+  commandNames: ['tsureteike', 'hunt'],
+  description:
+    '「この中に〜はいるか 連れて行け」\nex.) `!tsureteike プログラマ Rustは知っているか? ゲームですか? 錆のこと? 🦀`',
+  flagsKeys: [],
+  optionsKeys: [],
+  errorMessage: '構文ミスだ、問答無用で連れて行け',
+  generate(args) {
+    const [target, question, a, b, c] = args.body.split(' ');
+    const option = {
+      target,
+      question,
+      a,
+      b,
+      c
+    };
+    return makeTureteike(option);
+  }
+};
+
+interface TsureteikeArgs {
+  target: string;
+  question: string;
+  a: string;
+  b: string;
+  c: string;
+}
+
+function makeTureteike({ target, question, a, b, c }: TsureteikeArgs) {
+  return `「この中に${target}はいるか」\nA,B,C「いません」\n「${question}」\nA「${a}」\nB「${b}」\nC「${c}」\n「いたぞ、Cを連れて行け」`;
+}
