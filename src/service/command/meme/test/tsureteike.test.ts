f0615ec6dad0ab10d0f3b44ab8ffa10e230c3d10
m2en <me@m2en.dev>
2023-01-21 20:20:33 +0900

    feat: meme test-case

---
+++ src/service/command/meme/test/tsureteike.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from 'vitest';
+import { Meme } from '../../meme.js';
+
+import { createMockMessage } from '../../command-message.js';
+import { parseStringsOrThrow } from '../../../../adaptor/proxy/command/schema.js';
+
+describe('meme', () => {
+  const responder = new Meme();
+
+  it('use case of tsureteike', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(
+          [
+            'tsureteike',
+            'プログラマ',
+            'Rustは知っているか?',
+            'ゲームですか?',
+            '錆のこと?',
+            '🦀'
+          ],
+          responder.schema
+        ),
+        (message) => {
+          expect(message).toStrictEqual({
+            description:
+              '「この中にプログラマはいるか」\nA,B,C「いません」\n「Rustは知っているか?」\nA「ゲームですか?」\nB「錆のこと?」\nC「🦀」\n「いたぞ、Cを連れて行け」'
+          });
+        }
+      )
+    );
+  });
+
+  it('args null (tsureteike)', async () => {
+    await responder.on(
+      createMockMessage(
+        parseStringsOrThrow(['tsureteike'], responder.schema),
+        (message) => {
+          expect(message).toStrictEqual({
+            title: '引数が不足してるみたいだ。',
+            description: '構文ミスだ、問答無用で連れて行け'
+          });
+        }
+      )
+    );
+  });
+});
