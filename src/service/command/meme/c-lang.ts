d2bcd401b5ee8fc31bae05cf5dc7cbd75fb1f366
m2en <me@m2en.dev>
2023-02-08 01:37:49 +0900

    chore: change error message

--- src/service/command/meme/c-lang.ts
+++ src/service/command/meme/c-lang.ts
@@ -1,13 +1,13 @@
-import type {MemeTemplate} from "../../../model/meme-template.js";
+import type { MemeTemplate } from '../../../model/meme-template.js';
 
 export const cLang: MemeTemplate<never, never> = {
-    commandNames: ["clang", "c"],
-    description: "〜の天才\n9つの〜を操る",
-    flagsKeys: [],
-    optionsKeys: [],
-    errorMessage: 'memeの天才\n9つの構文を操る',
-    generate(args) {
-        const [option1, option2] = args.body.split(' ')
-        return `${option1}の天才\n9つの${option2}を操る`
-    }
-}
+  commandNames: ['clang', 'c'],
+  description: '〜の天才\n9つの〜を操る',
+  flagsKeys: [],
+  optionsKeys: [],
+  errorMessage: 'エラーの天才\n9つの引数エラーを操る',
+  generate(args) {
+    const [option1, option2] = args.body.split(' ');
+    return `${option1}の天才\n9つの${option2}を操る`;
+  }
+};
