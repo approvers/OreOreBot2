53061c48a7dd1e9974d2f704db178f636aebec4c
m2en <me@m2en.dev>
2023-02-09 02:00:14 +0900

    feat: add channelid fetch

---
+++ src/service/command/channel-info.ts
@@ -0,0 +1,54 @@
+import type {
+  BaseChannelStats,
+  ThreadChannelStats,
+  VoiceChannelStats
+} from '../../model/channel.js';
+import type {
+  CommandMessage,
+  CommandResponder,
+  HelpInfo
+} from './command-message.js';
+
+export interface ChannelStatsRepository {
+  fetchBaseChannelStats(channelId: string): Promise<BaseChannelStats | null>;
+  fetchVoiceChannelStats(channelId: string): Promise<VoiceChannelStats | null>;
+  fetchThreadChannelStats(
+    channelId: string
+  ): Promise<ThreadChannelStats | null>;
+}
+
+const SCHEMA = {
+  names: ['channel', 'chinfo', 'channelinfo'],
+  subCommands: {},
+  params: [
+    {
+      type: 'CHANNEL',
+      name: 'チャンネルID',
+      description: 'このIDのチャンネルを調べるよ'
+    }
+  ]
+} as const;
+
+export class ChannelInfo implements CommandResponder<typeof SCHEMA> {
+  help: Readonly<HelpInfo> = {
+    title: 'チャンネル秘書艦',
+    description: '指定したチャンネルの情報を調べてくるよ'
+  };
+
+  readonly schema = SCHEMA;
+
+  constructor(private readonly repo: ChannelStatsRepository) {}
+
+  async on(message: CommandMessage<typeof SCHEMA>): Promise<void> {
+    const [channelId] = message.args.params;
+
+    const baseStats = await this.repo.fetchBaseChannelStats(channelId);
+    if (!baseStats) {
+      await message.reply({
+        title: '引数エラー',
+        description: '指定したIDのチャンネルが見つからないみたい……'
+      });
+      return;
+    }
+  }
+}
