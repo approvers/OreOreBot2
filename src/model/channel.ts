15fc11b2547bc755d7f226d4a931dd854c5fa55f
m2en <me@m2en.dev>
2023-02-09 23:08:47 +0900

    docs: remove url

--- src/model/channel.ts
+++ src/model/channel.ts
@@ -1,11 +1,4 @@
-/**
- * 限界開発鯖はコミュニティサーバーではないので以下のチャンネルは2023-02-09現在利用できないため、個別のStatsは生やさない。
- * アナウンスチャンネル, アナウンスチャンネル(スレッド), ディレクトリチャンネル, フォーラムチャンネル, ステージチャンネル
- *
- * また、チャンネル検索の仕様上 DM は検索できないためこちらも対応しない
- */
-
-// channel#Channel-Types - https://discord.com/developers/docs/resources/channel#channel-object-channel-types
+// 全チャンネルタイプ
 export type ChannelType =
   | 'Text' // ID: 0
   | 'Voice' // ID: 2
