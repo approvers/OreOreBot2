ac6148b0d12a0596a755ce76afe7f5a6c15dbc2b
m2en <me@m2en.dev>
2023-02-09 15:11:59 +0900

    feat: add manager

---
+++ src/adaptor/discord/channel.ts
@@ -0,0 +1,45 @@
+import type { Client, GuildBasedChannel } from 'discord.js';
+
+import type { BaseChannelStats, ChannelType } from '../../model/channel.js';
+import type { Snowflake } from '../../model/id.js';
+import type { ChannelStatsRepository } from '../../service/command/channel-info.js';
+
+const mappingChannelTypes: Record<GuildBasedChannel['type'], ChannelType> = {
+  0: 'テキストチャンネル',
+  2: 'ボイスチャンネル',
+  4: 'カテゴリー',
+  5: 'アナウンスチャンネル',
+  10: 'アナウンスチャンネル(スレッド)',
+  11: '公開スレッド(パブリックスレッド)',
+  12: '非公開スレッド(プライベートスレッド)',
+  13: 'ステージチャンネル',
+  15: 'フォーラムチャンネル'
+};
+
+export class DiscordChannelManager implements ChannelStatsRepository {
+  constructor(
+    private readonly client: Client,
+    private readonly guildId: Snowflake
+  ) {}
+
+  async fetchStats(channelId: string): Promise<BaseChannelStats | null> {
+    const guild = await this.client.guilds.fetch(this.guildId);
+    if (!guild.available) {
+      throw new Error('guild unavailable');
+    }
+
+    const channel = await guild.channels.fetch(channelId);
+    if (!channel) {
+      return null;
+    }
+
+    return {
+      name: channel.name,
+      createAt: channel.createdAt,
+      url: channel.url,
+      type: mappingChannelTypes[channel.type],
+      manageable: channel.manageable,
+      viewable: channel.viewable
+    };
+  }
+}
