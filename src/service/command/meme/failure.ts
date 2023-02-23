aa1d53f0bd3e1c6bdaec6d010db0d13ae07b3e9c
m2en <me@m2en.dev>
2023-02-24 08:02:02 +0900

    feat: 共同通信記者の発言の "失敗" の部分を変えられるオプションを追加 (#746)

--- src/service/command/meme/failure.ts
+++ src/service/command/meme/failure.ts
@@ -2,14 +2,20 @@ import type { MemeTemplate } from '../../../model/meme-template.js';
 
 const sourceLink = 'https://dic.nicovideo.jp/id/5671528';
 
-export const failure: MemeTemplate<never, never> = {
+const failureOption = ['k'] as const;
+
+export const failure: MemeTemplate<never, (typeof failureOption)[number]> = {
   commandNames: ['failure', 'fail'],
-  description: `「〜〜〜」\n「わかりました。それは一般に失敗と言います、ありがとうございます」\n[元ネタ](${sourceLink})'`,
+  description: `「〜〜〜」\n「わかりました。それは一般に失敗と言います、ありがとうございます」\n* \`-k <失敗部分> <説明>\` で失敗部分を変更できます。 \n [元ネタ](${sourceLink})`,
   flagsKeys: [],
-  optionsKeys: [],
+  optionsKeys: failureOption,
   errorMessage:
     '「わかりました。それは一般に引数エラーと言います、ありがとうございます」',
   generate(args) {
-    return `「${args.body}」\n「わかりました。それは一般に失敗と言います、ありがとうございます」`;
+    const failureArgs = {
+      explanation: args.body,
+      impoliteness: args.options.k ?? '失敗'
+    };
+    return `「${failureArgs.explanation}」\n「わかりました。それは一般に${failureArgs.impoliteness}と言います、ありがとうございます」`;
   }
 };
