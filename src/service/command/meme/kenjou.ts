19e06af0faa0c0b2c35b4d7fce2aada2e7c51afb
m2en <me@m2en.dev>
2023-03-01 23:33:16 +0900

    fix: fix title

--- src/service/command/meme/kenjou.ts
+++ src/service/command/meme/kenjou.ts
@@ -1,5 +1,6 @@
 import type { MemeTemplate } from '../../../model/meme-template.js';
 
+const sourceTitle = '健常者エミュレータ事例集';
 const sourceLink = 'https://healthy-person-emulator.org/';
 
 const positionalKeys = ['title'] as const;
@@ -10,12 +11,11 @@ export const kenjou: MemeTemplate<
   (typeof positionalKeys)[number]
 > = {
   commandNames: ['kenjou'],
-  description: `[健常者エミュレーターWiki](${sourceLink})の構文ジェネレーター。\n健常者エミュレーターWikiにありそうなタイトルを指定すればうまくいきます。`,
+  description: `[${sourceTitle}](${sourceLink})の構文ジェネレーター。\n${sourceTitle}にありそうなタイトルを指定すればうまくいきます。`,
   pageName: 'kenjou',
   requiredPositionalKeys: positionalKeys,
-  errorMessage:
-    'はらちょのミーム機能を使うときは引数を忘れない方がいい - 健常者エミュレータ事例集Wiki',
+  errorMessage: `はらちょのミーム機能を使うときは引数を忘れない方がいい - ${sourceTitle}`,
   generate(args) {
-    return `${args.requiredPositionals.title} - 健常者エミュレータ事例集Wiki`;
+    return `${args.requiredPositionals.title} - ${sourceTitle}`;
   }
 };
