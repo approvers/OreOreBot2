1cd04443d6f9d9453de39672b151046838a1edba
MikuroXina <ryosukadnak@gmail.com>
2023-08-12 11:45:55 +0900

    Handle interactionCreate event

---
+++ src/adaptor/proxy/command/slash.ts
@@ -0,0 +1,126 @@
+import type {
+  ChatInputCommandInteraction,
+  CommandInteractionOptionResolver
+} from 'discord.js';
+
+import type {
+  ParsedSchema,
+  ParseError,
+  Param,
+  ParamsValues,
+  ParsedSubCommand,
+  SubCommands,
+  Schema,
+  SubCommandGroup,
+  SubCommand,
+  Params
+} from '../../../model/command-schema.js';
+
+export const parseOptions = <S extends Schema>(
+  interaction: ChatInputCommandInteraction,
+  schema: S
+): ['Ok', ParsedSchema<S>] | ['Err', ParseError] => {
+  const hasSubCommand =
+    Object.getOwnPropertyNames(schema.subCommands).length !== 0;
+
+  if (!hasSubCommand) {
+    if (schema.params === undefined) {
+      return [
+        'Ok',
+        {
+          name: interaction.commandName,
+          params: []
+        } as ParsedSchema<S>
+      ];
+    }
+    const params = parseParams(interaction.options, schema.params);
+    if (params[0] === 'Err') {
+      return params;
+    }
+    return [
+      'Ok',
+      {
+        name: interaction.commandName,
+        params: params[1]
+      }
+    ];
+  }
+  const subCommand = parseSubcommand(interaction.options, schema);
+  if (subCommand[0] === 'Err') {
+    return subCommand;
+  }
+  return [
+    'Ok',
+    {
+      name: interaction.commandName,
+      params: [],
+      subCommand: subCommand[1]
+    } as ParsedSchema<S>
+  ];
+};
+
+const parseParams = <S extends Schema | SubCommand>(
+  options: Omit<CommandInteractionOptionResolver, 'getMessage' | 'getFocused'>,
+  params: readonly Param[]
+): ['Ok', ParamsValues<Params<S>>] | ['Err', ParseError] => {
+  const result: unknown[] = [];
+  for (const param of params) {
+    switch (param.type) {
+      case 'BOOLEAN': {
+        const val = options.getBoolean(param.name, false) ?? param.defaultValue;
+        if (val === undefined) {
+          return ['Err', ['NEED_MORE_ARGS']];
+        }
+        result.push(val);
+        break;
+      }
+    }
+  }
+  return ['Ok', result as ParamsValues<Params<S>>];
+};
+
+const parseSubcommand = <S extends Schema | SubCommandGroup>(
+  options: Omit<CommandInteractionOptionResolver, 'getMessage' | 'getFocused'>,
+  schema: S
+): ['Ok', ParsedSubCommand<SubCommands<S>>] | ['Err', ParseError] => {
+  const subcommand = options.getSubcommand(false);
+  const subcommandGroup = options.getSubcommandGroup(false);
+
+  for (const name in schema.subCommands) {
+    if (name === subcommandGroup) {
+      const groupSchema = schema.subCommands[name] as SubCommandGroup;
+      const parsedSubSubCommand = parseSubcommand(options, groupSchema);
+      if (parsedSubSubCommand[0] === 'Err') {
+        return parsedSubSubCommand;
+      }
+      return [
+        'Ok',
+        {
+          name,
+          type: 'SUB_COMMAND',
+          subCommand: parsedSubSubCommand[1]
+        } as unknown as ParsedSubCommand<SubCommands<S>>
+      ];
+    }
+    if (name === subcommand) {
+      const subCommandSchema = schema.subCommands[name] as SubCommand;
+      const parsedParams = parseParams(options, subCommandSchema.params ?? []);
+      if (parsedParams[0] === 'Err') {
+        return parsedParams;
+      }
+      return [
+        'Ok',
+        {
+          name,
+          type: 'PARAMS',
+          params: parsedParams[1]
+        } as unknown as ParsedSubCommand<SubCommands<S>>
+      ];
+    }
+  }
+
+  return [
+    'Err',
+    ['UNKNOWN_COMMAND', [...Object.keys(schema.subCommands)], subcommand]
+  ];
+};
