0270a5f5cd5d57d54cf7d3e6be99fc0285020977
MikuroXina <ryosukadnak@gmail.com>
2023-08-14 00:16:27 +0900

    Update test sample

--- src/adaptor/proxy/command/slash.test.ts
+++ src/adaptor/proxy/command/slash.test.ts
@@ -5,7 +5,8 @@ import { parseOptions } from './slash.js';
 
 test('no args', () => {
   const SERVER_INFO_SCHEMA = {
-    names: ['serverinfo'],
+    names: ['guildinfo', 'serverinfo', 'guild', 'server'],
+    description: '限界開発鯖の情報を持ってくるよ',
     subCommands: {}
   } as const;
 
@@ -25,42 +26,48 @@ test('no args', () => {
 });
 
 test('single arg', () => {
-  const TIME_OPTION = [
+  const TIME_OPTIONS = [
     { name: 'at', description: '', type: 'STRING' }
   ] as const;
   const KAERE_SCHEMA = {
     names: ['kaere'],
+    description: 'VC内の人類に就寝を促すよ',
     subCommands: {
-      start: {
-        type: 'SUB_COMMAND'
-      },
       bed: {
         type: 'SUB_COMMAND_GROUP',
+        description: '強制切断モードを取り扱うよ',
         subCommands: {
           enable: {
-            type: 'SUB_COMMAND'
+            type: 'SUB_COMMAND',
+            description: '強制切断モードを有効化するよ'
           },
           disable: {
-            type: 'SUB_COMMAND'
+            type: 'SUB_COMMAND',
+            description: '強制切断モードを無効化するよ'
           },
           status: {
-            type: 'SUB_COMMAND'
+            type: 'SUB_COMMAND',
+            description: '現在の強制切断モードの設定を確認するよ'
           }
         }
       },
       reserve: {
         type: 'SUB_COMMAND_GROUP',
+        description: '予約システムを取り扱うよ',
         subCommands: {
           add: {
             type: 'SUB_COMMAND',
-            params: TIME_OPTION
+            description: '指定の時刻で予約するよ',
+            params: TIME_OPTIONS
           },
           cancel: {
             type: 'SUB_COMMAND',
-            params: TIME_OPTION
+            description: '指定時刻の予約をキャンセルするよ',
+            params: TIME_OPTIONS
           },
           list: {
-            type: 'SUB_COMMAND'
+            type: 'SUB_COMMAND',
+            description: '現在の予約を一覧するよ'
           }
         }
       }
@@ -140,10 +147,20 @@ test('single arg', () => {
 test('multi args', () => {
   const ROLE_CREATE_SCHEMA = {
     names: ['rolecreate'],
+    description: 'ロールを作成するよ',
     subCommands: {},
     params: [
-      { type: 'USER', name: 'target', description: '' },
-      { type: 'STRING', name: 'color', description: '', defaultValue: 'random' }
+      {
+        type: 'STRING',
+        name: 'name',
+        description: '作成するロールの名前を指定してね'
+      },
+      {
+        type: 'STRING',
+        name: 'color',
+        description:
+          '作成するロールの色を[HEX](https://htmlcolorcodes.com/)で指定してね'
+      }
     ]
   } as const;
 
