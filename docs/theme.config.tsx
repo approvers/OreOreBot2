bfe22c6f4e345817b6c554bd1b3ab7b189817436
Rintaro Itokawa <me@re-taro.dev>
2023-08-17 01:02:46 +0900

    docs: faviconの追加

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -42,6 +42,20 @@ const themeConfig: DocsThemeConfig = {
     <>
       <meta property="og:title" content="OreOreBot2 Documents" />
       <meta property="og:description" content={pkg.description} />
+      <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/assets/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/assets/favicon-16x16.png"
+      />
+      <link rel="icon" href="/assets/favicon.ico" />
     </>
   ),
   useNextSeoProps() {
