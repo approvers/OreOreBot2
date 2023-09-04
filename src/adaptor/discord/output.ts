b5cfdebcb3fedb03924a3053acadf7af7e747506
m1sk9 <me@m1sk9.dev>
2023-09-04 21:45:43 +0900

    feat: entrance output interface

--- src/adaptor/discord/output.ts
+++ src/adaptor/discord/output.ts
@@ -1,10 +1,10 @@
 import { ChannelType, type Client } from 'discord.js';
 
 import type { EmbedMessage } from '../../model/embed-message.js';
-import type { StandardOutput } from '../../service/output.js';
+import type { EntranceOutput, StandardOutput } from '../../service/output.js';
 import { convertEmbed } from '../embed-convert.js';
 
-export class DiscordOutput implements StandardOutput {
+export class DiscordStandardOutput implements StandardOutput {
   constructor(
     private readonly client: Client,
     private readonly channelId: string
@@ -20,5 +20,35 @@ export class DiscordOutput implements StandardOutput {
     await channel.send({
       embeds: [made]
     });
+  }
+}
+
+export class DiscordEntranceOutput implements EntranceOutput {
+  constructor(
+    private readonly client: Client,
+    private readonly channelId: string
+  ) {}
+
+  async sendEmbed(embed: EmbedMessage): Promise<void> {
+    const channel = await this.client.channels.fetch(this.channelId);
+
+    if (!channel || channel.type !== ChannelType.GuildText) {
+      throw new Error(`the channel (${this.channelId}) is not text channel`);
+    }
+
+    const made = convertEmbed(embed);
+    await channel.send({
+      embeds: [made]
+    });
+  }
+
+  async sendMention(userId: string): Promise<void> {
+    const channel = await this.client.channels.fetch(this.channelId);
+
+    if (!channel || channel.type !== ChannelType.GuildText) {
+      throw new Error(`the channel (${this.channelId}) is not text channel`);
+    }
+
+    await channel.send(`<@${userId}>`);
   }
 }
--- src/service/output.ts
+++ src/service/output.ts
@@ -1,5 +1,17 @@
 import type { EmbedMessage } from '../model/embed-message.js';
 
+/**
+ * "#無法地帯" に埋め込みを送信する interface.
+ */
 export interface StandardOutput {
+  sendEmbed(embed: EmbedMessage): Promise<void>;
+}
+
+/**
+ * "#玄関" に埋め込みを送信する interface.
+ */
+export interface EntranceOutput {
+  // 玄関でメンションを送信する
+  sendMention(userId: string): Promise<void>;
   sendEmbed(embed: EmbedMessage): Promise<void>;
 }
