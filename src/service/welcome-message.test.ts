66b18019be1aa985f03e4c8a4d12b75b983381b5
m1sk9 <me@m1sk9.dev>
2023-09-04 23:45:21 +0900

    test: added test-case

---
+++ src/service/welcome-message.test.ts
@@ -0,0 +1,72 @@
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+import type { Snowflake } from '../model/id.js';
+import type { EntranceOutput } from './output.js';
+import { WelcomeMessage } from './welcome-message.js';
+
+describe('WelcomeMessage', () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  const output: EntranceOutput = {
+    sendEmbed: () => Promise.resolve(),
+    sendEmbedWithMention: () => Promise.resolve()
+  };
+  const responder = new WelcomeMessage(output);
+
+  it('send welcome message', async () => {
+    const sendEmbed = vi.spyOn(output, 'sendEmbedWithMention');
+
+    await responder.on('JOIN', {
+      userId: '586824421470109716' as Snowflake,
+      isBot: false
+    });
+
+    expect(sendEmbed).toHaveBeenCalledWith(
+      {
+        title: '***†WELCOME TO UNDERGROUND†***',
+        description:
+          '司令官。ようこそ、限界開発鯖へ\nまずはじめに知っていてほしいことを教えるよ\n詳しい説明は [鯖民向けドキュメント](https://docs.approvers.dev/reference/getting-started) を見てね',
+        fields: [
+          {
+            name: 'メンバーデータの追加',
+            value:
+              '限界開発鯖では各メンバーの情報をデータベースに保存し、それらを元に様々な機能を提供しているよ。\n限界開発鯖に参加したら、まずは自分のメンバーデータを追加してね\n',
+            inline: false
+          },
+          {
+            name: '自己紹介',
+            value:
+              '参加したら <#687977635132997634> で同じ司令官のみんなに自己紹介しよう',
+            inline: false
+          },
+          {
+            name: 'メインチャンネル',
+            value:
+              '限界開発鯖では以下のチャンネルがメインで使われているよ\n- <#690909527461199922>\n- <#891210643938611260>\n- <#683939861539192865>',
+            inline: false
+          },
+          {
+            name: 'Botの導入',
+            value:
+              '限界開発鯖では自分が開発したBotを導入できるよ。導入に関する詳しい説明は [Bot 製作ガイドライン](https://docs.approvers.dev/guideline/bot-create-guideline) を確認してね',
+            inline: false
+          }
+        ]
+      },
+      '586824421470109716'
+    );
+  });
+
+  it('skip welcome message (join a bot)', async () => {
+    const sendEmbed = vi.spyOn(output, 'sendEmbedWithMention');
+
+    await responder.on('JOIN', {
+      userId: '586824421470109716' as Snowflake,
+      isBot: true
+    });
+
+    expect(sendEmbed).not.toHaveBeenCalled();
+  });
+});
