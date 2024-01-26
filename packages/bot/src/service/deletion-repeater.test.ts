a8d0e6e2ca18ee54d22994b8d397297d89dda37e
aqyuki <yksk171118@gmail.com>
2024-01-26 16:43:46 +0900

    fix(test): resolve type errors from interface update

--- packages/bot/src/service/deletion-repeater.test.ts
+++ packages/bot/src/service/deletion-repeater.test.ts
@@ -3,10 +3,14 @@ import { expect, it, vi } from 'vitest';
 import { DeletionRepeater } from './deletion-repeater.js';
 
 it('react to deleted message', async () => {
+  const now = new Date();
+  now.setSeconds(now.getSeconds() - 10);
+
   const responder = new DeletionRepeater(() => false);
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Stop',
+    createdAt: now,
     sendEphemeralToSameChannel: (message) => {
       expect(message)
         .toEqual(`Babaさん、メッセージを削除しましたね？私は見ていましたよ。内容も知っています。
@@ -19,17 +23,24 @@ Wall Is Stop
 });
 
 it('must not react', async () => {
+  const now = new Date();
+  now.setSeconds(now.getSeconds() - 10);
+
   const responder = new DeletionRepeater(() => false);
   const fn = vi.fn();
   await responder.on('CREATE', {
     author: 'Baba',
     content: 'Wall Is Not Stop',
+    createdAt: now,
     sendEphemeralToSameChannel: fn
   });
   expect(fn).not.toHaveBeenCalled();
 });
 
 it("must not react if it's ignore target", async () => {
+  const now = new Date();
+  now.setSeconds(now.getSeconds() - 10);
+
   const responder = new DeletionRepeater(
     (content) => content === 'Wall Is Stop'
   );
@@ -37,18 +48,23 @@ it("must not react if it's ignore target", async () => {
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Stop',
+    createdAt: now,
     sendEphemeralToSameChannel: fn
   });
   expect(fn).not.toHaveBeenCalled();
 });
 
 it("must react if it's not ignore target", async () => {
+  const now = new Date();
+  now.setSeconds(now.getSeconds() - 10);
+
   const responder = new DeletionRepeater(
     (content) => content === 'Wall Is Stop'
   );
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Not Stop',
+    createdAt: now,
     sendEphemeralToSameChannel: (message) => {
       expect(message)
         .toEqual(`Babaさん、メッセージを削除しましたね？私は見ていましたよ。内容も知っています。
