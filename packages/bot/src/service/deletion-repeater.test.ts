b74066d74b30f1349abd5d755307b4c529f4ddad
aqyuki <yksk171118@gmail.com>
2024-01-26 17:39:04 +0900

    chore: utilize mock function during testing in DeletionRepeater constructor

--- packages/bot/src/service/deletion-repeater.test.ts
+++ packages/bot/src/service/deletion-repeater.test.ts
@@ -2,19 +2,29 @@ import { expect, it, vi } from 'vitest';
 
 import { DeletionRepeater } from './deletion-repeater.js';
 
-it('react to deleted message', async () => {
-  const now = new Date();
-  const createdAt = new Date();
-  createdAt.setSeconds(createdAt.getSeconds() - 10);
+const createdDate = () => {
+  const now = new Date(2024, 1, 1, 0, 0, 30);
+  return now;
+};
 
-  const responder = new DeletionRepeater(
-    () => false,
-    () => now
-  );
+const deletedDate = () => {
+  // createdDateよりも10秒後
+  const createdAt = new Date(2024, 1, 1, 0, 0, 40);
+  return createdAt;
+};
+
+const fastDeletedDate = () => {
+  // createdDateよりも1秒後
+  const createdAt = new Date(2024, 1, 1, 0, 0, 31);
+  return createdAt;
+};
+
+it('react to deleted message', async () => {
+  const responder = new DeletionRepeater(() => false, deletedDate);
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Stop',
-    createdAt: createdAt,
+    createdAt: createdDate(),
     sendEphemeralToSameChannel: (message) => {
       expect(message)
         .toEqual(`Babaさん、メッセージを削除しましたね？私は見ていましたよ。内容も知っています。
@@ -27,56 +37,41 @@ Wall Is Stop
 });
 
 it('must not react', async () => {
-  const now = new Date();
-  const createdAt = new Date();
-  createdAt.setSeconds(now.getSeconds() - 10);
-
-  const responder = new DeletionRepeater(
-    () => false,
-    () => now
-  );
+  const responder = new DeletionRepeater(() => false, deletedDate);
   const fn = vi.fn();
   await responder.on('CREATE', {
     author: 'Baba',
     content: 'Wall Is Not Stop',
-    createdAt: createdAt,
+    createdAt: createdDate(),
     sendEphemeralToSameChannel: fn
   });
   expect(fn).not.toHaveBeenCalled();
 });
 
 it("must not react if it's ignore target", async () => {
-  const now = new Date();
-  const createdAt = new Date();
-  createdAt.setSeconds(createdAt.getSeconds() - 10);
-
   const responder = new DeletionRepeater(
     (content) => content === 'Wall Is Stop',
-    () => now
+    deletedDate
   );
   const fn = vi.fn();
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Stop',
-    createdAt: createdAt,
+    createdAt: createdDate(),
     sendEphemeralToSameChannel: fn
   });
   expect(fn).not.toHaveBeenCalled();
 });
 
 it("must react if it's not ignore target", async () => {
-  const now = new Date();
-  const createdAt = new Date();
-  createdAt.setSeconds(createdAt.getSeconds() - 10);
-
   const responder = new DeletionRepeater(
     (content) => content === 'Wall Is Stop',
-    () => now
+    deletedDate
   );
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Not Stop',
-    createdAt: createdAt,
+    createdAt: createdDate(),
     sendEphemeralToSameChannel: (message) => {
       expect(message)
         .toEqual(`Babaさん、メッセージを削除しましたね？私は見ていましたよ。内容も知っています。
@@ -89,18 +84,11 @@ Wall Is Not Stop
 });
 
 it("react to deleted message if it's too fast", async () => {
-  const now = new Date();
-  const createdAt = new Date();
-  createdAt.setSeconds(now.getSeconds() - 1);
-
-  const responder = new DeletionRepeater(
-    () => false,
-    () => now
-  );
+  const responder = new DeletionRepeater(() => false, fastDeletedDate);
   await responder.on('DELETE', {
     author: 'Baba',
     content: 'Wall Is Stop',
-    createdAt: createdAt,
+    createdAt: createdDate(),
     sendEphemeralToSameChannel: (message) => {
       expect(message)
         .toEqual(`Babaさんの恐ろしく早いメッセージの削除。私じゃなきゃ見逃していましたよ。
