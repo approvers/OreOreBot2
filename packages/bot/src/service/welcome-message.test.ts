a90773b4ffbc8f600b14bd8495e78cc909af4f94
MikuroXina <ryosukadnak@gmail.com>
2024-02-28 11:23:48 +0900

    feat: Update welcome message

--- packages/bot/src/service/welcome-message.test.ts
+++ packages/bot/src/service/welcome-message.test.ts
@@ -26,12 +26,12 @@ describe('WelcomeMessage', () => {
       {
         title: '***†WELCOME TO UNDERGROUND†***',
         description:
-          '司令官。ようこそ、限界開発鯖へ\nまずはじめに知っていてほしいことを教えるよ\n詳しい説明は [鯖民向けドキュメント](https://docs.approvers.dev/reference/getting-started) を見てね',
+          '司令官。ようこそ、限界開発鯖へ\nまずはじめに知っていてほしいことを教えるよ\n詳しい説明は [鯖民向けドキュメント](https://approvers.notion.site/Getting-Started-cdef1e63f91f4168b3d071c0f1b9c41f) を見てね',
         fields: [
           {
             name: 'メンバーデータの追加',
             value:
-              '限界開発鯖では各メンバーの情報をデータベースに保存し、それらを元に様々な機能を提供しているよ。\n限界開発鯖に参加したら、まずは自分のメンバーデータを追加してね\n',
+              '限界開発鯖では各メンバーの情報をデータベースに保存し、それらを元に様々な機能を提供しているよ。\n限界開発鯖に参加したら、まずは[情報登録ウェブアプリ](https://members.approvers.dev/)で自分のメンバーデータを追加してね\n',
             inline: false
           },
           {
