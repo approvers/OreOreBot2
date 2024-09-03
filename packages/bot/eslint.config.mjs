5fbff67a7eceda3f0c1feb4ffd549afe9ddda8f5
MikuroXina <mikuroxina@gmail.com>
2024-09-03 19:36:08 +0900

    Migrate config

---
+++ packages/bot/eslint.config.mjs
@@ -0,0 +1,63 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import markdown from 'eslint-plugin-markdown';
+import tsdoc from 'eslint-plugin-tsdoc';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier'
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      markdown,
+      tsdoc
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: true
+      }
+    },
+
+    rules: {
+      'max-params': 'error',
+      'tsdoc/syntax': 'warn',
+
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowNumber: true
+        }
+      ]
+    }
+  },
+  {
+    files: ['**/*.md'],
+    processor: 'markdown/markdown'
+  }
+];
